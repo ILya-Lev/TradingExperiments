b58421a4b476265a443a897def6375c12d5cea07
Illia Levandovskyi <lis2889@gmail.com>
2024-09-03 10:50:43 +0200

    BinomialTreeModel is added to reflect Cox-Ross-Rubinstein model

---
+++ src/BlackScholesMerton/BinomialTreeModel.cs
@@ -0,0 +1,111 @@
+﻿namespace BlackScholesMerton;
+
+public class BinomialTreeModel
+{
+    public Node Root { get; }
+    public IReadOnlyList<Node> Leaves { get; }
+
+    /// <summary>
+    /// Martingale probability of going up the tree
+    /// i.e. S(i) = E[discount * S(i+1)] = discount * (q*S(i)*u + (1-q)*S(i)*d)
+    /// as the tree is binary (up/down moves only)
+    /// => q = (1/discount - d) / (u-d);
+    /// </summary>
+    public double Q { get; }
+
+    /// <summary> price change when going up the tree </summary>
+    public double U { get; }
+    
+    /// <summary> price change when going down the tree </summary>
+    public double D { get; }
+
+    /// <summary> a discount to be applied on each step for payoff calculation based on child nodes </summary>
+    public double Discount { get; }
+
+    /// <summary> payoff of a given node if executed right now - expects current node's Strike price only </summary>
+    public Func<double, double> Payoff { get; }
+
+    /// <summary>
+    /// calculates payoffs for each node of cox-Ross-Rubinstein model
+    /// </summary>
+    /// <param name="isEuropean">tells if the option could be exercised right now (false) or not (true), affects payoff usage</param>
+    /// <param name="n">number of steps</param>
+    /// <param name="s">initial strike price S(0)</param>
+    /// <param name="u">going up factor</param>
+    /// <param name="d">going down factor</param>
+    /// <param name="discount">generates discount factor after 1 step</param>
+    /// <param name="payoff">payoff of a given node if executed right now </param>
+    public BinomialTreeModel(bool isEuropean
+        , int n, double s, double u, double d
+        , double discount, Func<double, double> payoff)
+    {
+        if (!(u > 1 / discount && 1 / discount > d))
+            throw new Exception($"Invalid parameters - the relation is not followed: {u} > {1 / discount} > {d}");
+
+        U = u;
+        D = d;
+        Discount = discount;
+        Payoff = payoff;
+        Q = (1 / discount - d) / (u - d);
+        
+        Root = new() { Strike = s };
+        Leaves = BuildTreeAndAssignStrikePrices(n);
+
+        AssignPayoffs(GetPayoff);
+        double GetPayoff(Node node) => isEuropean ? GetEuropeanPayoff(node) : GetAmericanPayoff(node);
+    }
+
+    private double GetEuropeanPayoff(Node node) => Discount * (node.Up!.Payoff * Q + node.Down!.Payoff * (1 - Q));
+    private double GetAmericanPayoff(Node node) => Math.Max(Payoff(node.Strike), GetEuropeanPayoff(node));
+    
+    private List<Node> BuildTreeAndAssignStrikePrices(int n)
+    {
+        var layer = new List<Node>(){Root};
+        for (int step = 0; step < n; step++)
+        {
+            var nextLayer = new List<Node> { CreateUpChild(layer[0]) };
+            foreach (var node in layer)
+            {
+                node.Up = nextLayer.Last();
+                nextLayer.Last().ParentUp = node;
+
+                node.Down = CreateDownChild(node);
+                nextLayer.Add(node.Down);
+            }
+            layer = nextLayer;
+        }
+        return layer;
+    }
+
+    private Node CreateUpChild(Node node) => new() { ParentUp = node, Strike = node.Strike * U };
+    private Node CreateDownChild(Node node) => new() { ParentDown = node, Strike = node.Strike * D };
+
+    private void AssignPayoffs(Func<Node, double> payoff)
+    {
+        foreach (var leaf in Leaves)
+            leaf.Payoff = Payoff(leaf.Strike);
+
+        var layer = Leaves.ToList();
+        while (layer.Count > 1)
+        {
+            var previousLayer = new List<Node>();
+            foreach (var leaf in layer.Skip(1))
+            {
+                var node = leaf.ParentDown!;
+                node.Payoff = payoff(node);
+                previousLayer.Add(node);
+            }    
+            layer = previousLayer;
+        }
+    }
+
+    public record Node
+    {
+        public Node? ParentUp { get; set; }
+        public Node? ParentDown { get; set; }
+        public Node? Up { get; set; }
+        public Node? Down { get; set; }
+        public double Strike { get; init; }
+        public double Payoff { get; set; }
+    }
+}
---
+++ test/BlackScholesMerton.Tests/BinomialTreeModelTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace BlackScholesMerton.Tests;
+
+public class BinomialTreeModelTests
+{
+    [Fact]
+    public void Ctor_AmericanPutOption_MatchManualCalculations()
+    {
+        var treeModel = new BinomialTreeModel(false, 2, 100, 1.1, 0.9, 1 / 1.05
+                                            , (s) => Math.Max(0, 101 - s));
+        
+        using var scope = new AssertionScope();
+        treeModel.Q.Should().BeApproximately(0.75, 1e-2);
+        treeModel.Root.Payoff.Should().BeApproximately(2.9592, 1e-4);
+    }
+
+    [Fact]
+    public void Ctor_AmericanStrikeSquaredOption_MatchManualCalculations()
+    {
+        var treeModel = new BinomialTreeModel(false, 2, 100, 1.02, 0.98,1 / 1.01
+                                            , (s) => s > 101 ? s*s : 0);
+        
+        using var scope = new AssertionScope();
+        treeModel.Q.Should().BeApproximately(0.75, 1e-2);
+        treeModel.Root.Payoff.Should().BeApproximately(7725.7426, 1e-4);
+    }
+}
