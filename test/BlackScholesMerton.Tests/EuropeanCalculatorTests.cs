2e3e74d2cca5b0a25173345649d3581509060f42
Illia Levandovskyi <lis2889@gmail.com>
2024-09-06 23:15:59 +0200

    perfect hedging replication example in tests

--- test/BlackScholesMerton.Tests/EuropeanCalculatorTests.cs
+++ test/BlackScholesMerton.Tests/EuropeanCalculatorTests.cs
@@ -1,11 +1,19 @@
 using FluentAssertions;
 using FluentAssertions.Execution;
+using Xunit.Abstractions;
 
 namespace BlackScholesMerton.Tests;
 
 [Trait("Category", "Unit")]
 public class EuropeanCalculatorTests
 {
+    private readonly ITestOutputHelper _output;
+
+    public EuropeanCalculatorTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
     private const double Precision = 5e-5;
 
     [Fact]
@@ -41,4 +49,46 @@ public class EuropeanCalculatorTests
             new EuropeanCalculator(40 + i%10, 50, 0.05, 0.2, 20 / 52.0);
         }
     }
+
+    [Fact]
+    public void GetPricesAndGreeks_PerfectHedgingExample_MatchManualCalculations()
+    {
+        const double daysInYear = 252.0;
+        var stockPrices = new[]
+        {
+            55.5625, 55.3750, 55.4375, 56.5625, 59.1250, 60.3125, 61.3125, 60.6250,
+            62.6875, 61.25, 63.25, 64.1875, 64.25, 65, 63,
+            64.1875, 65.8125, 68.25, 68.125, 68.8125
+        };
+        stockPrices.Should().HaveCount(20);
+        
+        var dailyData = stockPrices[1..].Zip(stockPrices[..^1], (lhs, rhs) => Math.Log(lhs/rhs))
+            .ToArray();
+        var averageDailyData = dailyData.Average();
+        var sampleVariance = dailyData.Sum(d => Math.Pow(d - averageDailyData, 2)) / (dailyData.Length - 1);
+        var annualStdDev = Math.Sqrt(sampleVariance * daysInYear);
+        annualStdDev.Should().BeApproximately(0.317139, Precision);
+
+        var calculators = stockPrices
+            .Select((s, i) => new EuropeanCalculator(s, 55, 0, annualStdDev, (20-i)/daysInYear))
+            .ToArray();
+
+        var wealth = new double[calculators.Length];
+        wealth[0] = calculators[0].CallPrice;
+        for (int i = 1; i < calculators.Length; i++)
+        {
+            wealth[i] = wealth[i - 1] + calculators[i - 1].CallDelta * (calculators[i].S - calculators[i - 1].S);
+        }
+
+        using var scope = new AssertionScope();
+
+        _output.WriteLine("replication experiment");
+        _output.WriteLine("Time\tStockPrice\tCallPrice\tDelta\t\tWealth");
+        for (int i = 0; i < calculators.Length; i++)
+        {
+            var stat = calculators[i];
+            _output.WriteLine($"{i}\t\t{stat.S:N4}\t\t{stat.CallPrice:N4}\t\t{stat.CallDelta:N4}\t\t{wealth[i]:N4}");
+        }
+    }
+
 }
