95f3c70b4848f0129dbb314573f6cab3421880a5
Illia Levandovskyi <lis2889@gmail.com>
2024-09-19 21:35:39 +0200

    black76 - option on future - european call/put prices are calculated

---
+++ src/BlackScholesMerton/EuropeanOptionOnFuturesCalculator.cs
@@ -0,0 +1,71 @@
+﻿using MathNet.Numerics.Distributions;
+
+namespace BlackScholesMerton;
+
+public class EuropeanOptionOnFuturesCalculator
+{
+    /// <summary>
+    /// calculates fair call option on futures price in Black76 model
+    /// under risk-neutral probability Q
+    /// i.e. dependency over future price expected value is replaced with risk-free growth rate (bank - r)
+    /// as due to Girsanov theorem exists probability Q,
+    /// under which Brownian motion W_Q = W + (mu - r)/sigma * t
+    /// stock price is Martingale, i.e. E_Q[exp{-r*(T-t)}*S(T)] = S(t)
+    /// also see https://en.wikipedia.org/wiki/Black_model
+    /// </summary>
+    /// <param name="f">future price now</param>
+    /// <param name="k">strike price (price of the asset at the exercise)</param>
+    /// <param name="r">risk-free growth rate (bank account return rate)</param>
+    /// <param name="sigma">standard deviation of future price distribution (its volatility square root)</param>
+    /// <param name="t">time to maturity from now, in years</param>
+    /// <remarks>fair price indicates whether to buy (if above) or to sell (if below market)</remarks>
+    public EuropeanOptionOnFuturesCalculator(double f, double k, double r, double sigma, double t)
+    {
+        F = f;
+        K = k;
+        R = r;
+        Sigma = sigma;
+        T = t;
+
+        var sqrT = Math.Sqrt(T);
+
+        D1 = Math.Abs(F - K) < 1e-6
+            ? Sigma / 2 * sqrT
+            : Math.Log(F/K) /Sigma/sqrT + sqrT*Sigma/2;
+
+        D2 = D1 - Sigma * sqrT;
+
+        var nd2 = N(D2);
+        var ndm2 = 1 - nd2;
+        
+        CallDelta = N(D1);
+        PutDelta = CallDelta - 1;
+
+        var discount = Math.Exp(-R * T);
+        var discountedK = K * discount;
+
+        //Black76 call option price
+        CallPrice = discount * (F * CallDelta - K * nd2);
+
+        //according to call-put european options parity 
+        // c + k*exp(-r*t) = p + s
+        PutPrice = CallPrice + discountedK - F;
+    }
+
+    public double F { get; }
+    public double K { get; }
+    public double R { get; }
+    public double Sigma { get; }
+    public double T { get; }
+
+    public double D1 { get; }
+    public double D2 { get; }
+
+    public double CallPrice { get; }
+    public double PutPrice { get; }
+
+    public double CallDelta { get; }
+    public double PutDelta { get; }
+
+    private static double N(double x) => Normal.CDF(0, 1, x);
+}
---
+++ test/BlackScholesMerton.Tests/EuropeanOptionOnFuturesCalculator.cs
@@ -0,0 +1,61 @@
+﻿using Xunit.Abstractions;
+
+namespace BlackScholesMerton.Tests;
+
+public class EuropeanOptionOnFuturesCalculatorTests(ITestOutputHelper output)
+{
+    /// <summary>
+    /// scenario:
+    /// today future price is 100; you sell European call option on the future for 10% over the fair price with maturity 6m
+    /// then you hedge it with delta hedging
+    ///
+    /// in 1 month you re-balance your portfolio according to the new delta value; at the moment future price is 98
+    ///
+    /// in 2 month you liquidate the portfolio: sell the future position, buy back the option position and close bank position
+    ///
+    /// target question - what is your PNL (fees are ignored)
+    /// </summary>
+    [Fact]
+    public void EuropeanOption_1mReplicate_2mLiquidate_TotalPnl()
+    {
+        var calculators = new[]
+        {
+            new EuropeanOptionOnFuturesCalculator(100, 100, 0.05, 0.25, 1 / 2.0),
+            new EuropeanOptionOnFuturesCalculator(98, 100, 0.05, 0.25, 5 / 12.0),
+            new EuropeanOptionOnFuturesCalculator(101, 100, 0.05, 0.25, 1 / 3.0),
+        };
+        var riskFreeGrowthPerMonth = Math.Exp(0.05 / 12);
+
+        var optionInitial = 1.10 * calculators[0].CallPrice;
+        var buyFutureInitial = calculators[0].CallDelta * calculators[0].F;
+        var bankInitial = optionInitial - buyFutureInitial;
+
+        var reBalanceCost = (calculators[1].CallDelta - calculators[0].CallDelta) * calculators[1].F;
+        var bankReBalance = bankInitial * riskFreeGrowthPerMonth - reBalanceCost;
+
+        var liquidateOption = calculators[2].CallPrice;
+        var liquidateFutures = calculators[1].CallDelta * calculators[2].F;
+        var liquidateBank = bankReBalance * riskFreeGrowthPerMonth;
+
+        var pnl = -liquidateOption + liquidateFutures + liquidateBank;
+
+        output.WriteLine(
+            $"""
+             initial:
+                     option cost {optionInitial:N4}
+                     futures # {calculators[0].CallDelta:N4}
+                     buy futures {buyFutureInitial:N4}
+                     bank {bankInitial:N4}
+             re balance:
+                     futures # {calculators[1].CallDelta:N4}
+                     buy futures {reBalanceCost:N4}
+                     bank {bankReBalance:N4}
+             liquidate:
+                     option {liquidateOption:N4}
+                     sell futures {liquidateFutures:N4}
+                     bank {liquidateBank:N4}
+             pnl: {pnl:N4}
+             """
+        );
+    }
+}
