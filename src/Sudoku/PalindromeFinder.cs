f2eb4e3fc92cf9932efcd1e20d9a6034a544a5b1
Illia Levandovskyi <lis2889@gmail.com>
2024-09-29 20:03:59 +0200

    max 6 digit palindrome finder

---
+++ src/Sudoku/PalindromeFinder.cs
@@ -0,0 +1,33 @@
+﻿namespace Sudoku;
+
+public class PalindromeFinder
+{
+    public static int FindLargestPalindrome()
+    {
+        return Enumerable.Range(1, 1000)
+            .SelectMany(lhs => Enumerable.Range(lhs, 1000 - lhs)
+                .Select(rhs => lhs * rhs))
+            .Reverse()
+            .Where(IsPalindrome)
+            .Max();
+    }
+
+    public static bool IsPalindrome(int n)
+    {
+        var digits = new List<int>();
+        while (n > 0)
+        {
+            digits.Add(n % 10);
+            n /= 10;
+        }
+
+        for (int i = 0; i < digits.Count/2; i++)
+        {
+            if (digits[i] != digits[^(i+1)])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ test/Sudoku.Tests/PalindromeFinderTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace Sudoku.Tests;
+
+public class PalindromeFinderTests
+{
+    [Fact]
+    public void FindLargestPalindrome_Observe()
+    {
+        PalindromeFinder.FindLargestPalindrome().Should().Be(906609);
+    }
+
+    [Fact]
+    public void IsPalindrome_Observe()
+    {
+        PalindromeFinder.IsPalindrome(906609).Should().BeTrue();
+    }
+}
