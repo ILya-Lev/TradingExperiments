263873ad98de5d3152d075c5f1312118d90d70ba
Illia Levandovskyi <lis2889@gmail.com>
2024-10-22 22:18:15 +0200

    running search values demo on different machine - same code, larger absolute values

--- benchmark/Sudoku.Benchmarks/SearchStringPerformance.cs
+++ benchmark/Sudoku.Benchmarks/SearchStringPerformance.cs
@@ -65,5 +65,38 @@ public class SearchStringPerformance
      SearchStringPerformance.FindAll: .NET 8.0 -> 1 outlier  was  removed, 5 detected (9.74 ns..10.84 ns, 20.37 ns)
      SearchStringPerformance.FindAll: .NET 8.0 -> 5 outliers were detected (27.12 ns..28.09 ns)
    
- 
+// * Summary *
+   
+   BenchmarkDotNet v0.14.0, Windows 11 (10.0.22631.4317/23H2/2023Update/SunValley3)
+   Intel Core i7-8750H CPU 2.20GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores
+   .NET SDK 9.0.100-preview.4.24267.66
+     [Host]   : .NET 8.0.10 (8.0.1024.46610), X64 RyuJIT AVX2
+     .NET 8.0 : .NET 8.0.10 (8.0.1024.46610), X64 RyuJIT AVX2
+   
+   Job=.NET 8.0  Runtime=.NET 8.0
+   
+   | Method          | Input                | Mean       | Error     | StdDev     | Median     | Gen0   | Allocated |
+   |---------------- |--------------------- |-----------:|----------:|-----------:|-----------:|-------:|----------:|
+   | FindAll         | 1abcd                |  17.782 ns | 0.3804 ns |  0.5693 ns |  17.535 ns | 0.0068 |      32 B |
+   | FindSearchValue | 1abcd                |   3.332 ns | 0.0983 ns |  0.1410 ns |   3.273 ns |      - |         - |
+   | FindHashSet     | 1abcd                |  13.432 ns | 0.2932 ns |  0.3707 ns |  13.428 ns | 0.0068 |      32 B |
+   | FindAll         | aaaaa(...)aaaa0 [49] | 416.405 ns | 8.3258 ns | 20.5793 ns | 414.581 ns | 0.0067 |      32 B |
+   | FindSearchValue | aaaaa(...)aaaa0 [49] |   6.098 ns | 0.1544 ns |  0.3085 ns |   6.029 ns |      - |         - |
+   | FindHashSet     | aaaaa(...)aaaa0 [49] | 313.272 ns | 6.0918 ns |  6.5182 ns | 311.137 ns | 0.0067 |      32 B |
+   | FindAll         | abcd1                |  48.888 ns | 0.9918 ns |  1.5145 ns |  48.549 ns | 0.0068 |      32 B |
+   | FindSearchValue | abcd1                |   7.300 ns | 0.1729 ns |  0.4703 ns |   7.163 ns |      - |         - |
+   | FindHashSet     | abcd1                |  40.734 ns | 0.8388 ns |  1.8413 ns |  40.470 ns | 0.0068 |      32 B |
+   
+   // * Warnings *
+   MultimodalDistribution
+     SearchStringPerformance.FindAll: .NET 8.0 -> distribution is bimodal (mValue = 3.48)
+   
+   // * Hints *
+   Outliers
+     SearchStringPerformance.FindAll: .NET 8.0         -> 1 outlier  was  removed (21.14 ns)
+     SearchStringPerformance.FindSearchValue: .NET 8.0 -> 3 outliers were removed (5.39 ns..5.51 ns)
+     SearchStringPerformance.FindHashSet: .NET 8.0     -> 1 outlier  was  removed (16.39 ns)
+     SearchStringPerformance.FindAll: .NET 8.0         -> 3 outliers were removed (490.53 ns..562.08 ns)
+     SearchStringPerformance.FindSearchValue: .NET 8.0 -> 7 outliers were removed (10.33 ns..12.51 ns)
+     SearchStringPerformance.FindHashSet: .NET 8.0     -> 3 outliers were removed (49.76 ns..55.28 ns)
  */
