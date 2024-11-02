b4d3767b2f04d95da0b50eff7201aa50afbddcc9
Illia Levandovskyi <lis2889@gmail.com>
2024-11-02 20:26:15 +0100

    improving result pattern impl

--- src/ResultLib/Result.cs
+++ src/ResultLib/Result.cs
@@ -2,50 +2,54 @@
 
 public class Result<T>
 {
-    public T? Value { get; init; }
-    public string? Error { get; init; }
-}
+    public T? Value { get; }
+    public string? Error { get; }
+    public bool IsSuccess { get; }
 
-public class Result
-{
-    public static Result<T> Create<T>(T? value = default(T?), string? error = null)
+    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Fail(string error) => new(error: error);
+    public static implicit operator Result<T>(T value) => Success(value);
+
+    private Result(T? value = default(T?), string? error = null)
     {
         if (value is null && error is null)
             throw new InvalidOperationException($"it is not allowed to have both value and error null");
         if (value is not null && error is not null)
             throw new InvalidOperationException($"it is not allowed to have both value and error not null");
 
-        return new Result<T> { Value = value, Error = error };
+        Value = value;
+        Error = error;
+        IsSuccess = error is null;
     }
 }
 
 public static class ResultExtensions
 {
-    public static Result<V> Transform<U, V>(this Result<U> current, Func<U, V> projection)
+    public static Result<V> Apply<U, V>(this Result<U> current, Func<U, V> projection)
     {
         if (current.Error is not null)
-            return Result.Create<V>(error: current.Error);
-        try
-        {
-            return Result.Create(value: projection(current.Value!));
-        }
-        catch (Exception exc)
-        {
-            return Result.Create<V>(error: exc.ToString());
-        }
-    }
-    
-    public static Result<V> Transform<U, V>(this Result<U> current, Func<U, Result<V>> projection)
-    {
-        if (current.Error is not null)
-            return Result.Create<V>(error: current.Error);
+            return Result<V>.Fail(current.Error);
         try
         {
             return projection(current.Value!);
         }
         catch (Exception exc)
         {
-            return Result.Create<V>(error: exc.ToString());
+            return Result<V>.Fail(exc.ToString());
+        }
+    }
+    
+    public static Result<V> Apply<U, V>(this Result<U> current, Func<U, Result<V>> projection)
+    {
+        if (current.Error is not null)
+            return Result<V>.Fail(current.Error);
+        try
+        {
+            return projection(current.Value!);
+        }
+        catch (Exception exc)
+        {
+            return Result<V>.Fail(exc.ToString());
         }
     }
 
@@ -60,21 +64,21 @@ public static class ResultExtensions
         }
         catch (Exception exc)
         {
-            return Result.Create<T>(error: exc.ToString());
+            return Result<T>.Fail(exc.ToString());
         }
     }
 
     public static async Task<Result<V>> TransformAsync<U, V>(this Result<U> current, Func<U, Task<V>> projection)
     {
         if (current.Error is not null)
-            return Result.Create<V>(error: current.Error);
+            return Result<V>.Fail(current.Error);
         try
         {
-            return Result.Create(value: await projection(current.Value!));
+            return await projection(current.Value!);
         }
         catch (Exception exc)
         {
-            return Result.Create<V>(error: exc.ToString());
+            return Result<V>.Fail(exc.ToString());
         }
     }
 
@@ -82,14 +86,14 @@ public static class ResultExtensions
     {
         var c = await current;
         if (c.Error is not null)
-            return Result.Create<V>(error: c.Error);
+            return Result<V>.Fail(c.Error);
         try
         {
-            return Result.Create(value: await projection(c.Value!));
+            return await projection(c.Value!);
         }
         catch (Exception exc)
         {
-            return Result.Create<V>(error: exc.ToString());
+            return Result<V>.Fail(exc.ToString());
         }
     }
 
@@ -104,7 +108,7 @@ public static class ResultExtensions
         }
         catch (Exception exc)
         {
-            return Result.Create<T>(error: exc.ToString());
+            return Result<T>.Fail(exc.ToString());
         }
     }
 
@@ -120,11 +124,39 @@ public static class ResultExtensions
         }
         catch (Exception exc)
         {
-            return Result.Create<T>(error: exc.ToString());
+            return Result<T>.Fail(exc.ToString());
         }
     }
 
     public static Func<R> Curry<T1, R>(this Func<T1, R> f, T1 v) => () => f(v);
     public static Func<T2, R> Curry<T1, T2, R>(this Func<T1, T2, R> f, T1 v) => v2 => f(v, v2);
     public static Func<T2, T3, R> Curry<T1, T2, T3, R>(this Func<T1, T2, T3, R> f, T1 v) => (v2, v3) => f(v, v2, v3);
+
+    public static Func<U, Result<V>> CurryApply<T, U, V>(this Result<T> current, Func<T, U, Result<V>> projection)
+    {
+        if (current.Error is not null)
+            return (_) => Result<V>.Fail(current.Error);
+        try
+        {
+            return (u) => projection(current.Value!, u);
+        }
+        catch (Exception exc)
+        {
+            return (_) => Result<V>.Fail(exc.ToString());
+        }
+    }
+
+    public static Func<T, Result<V>> CurryApply<T, U, V>(this Result<U> current, Func<T, U, Result<V>> projection)
+    {
+        if (current.Error is not null)
+            return (_) => Result<V>.Fail(current.Error);
+        try
+        {
+            return (u) => projection(u, current.Value!);
+        }
+        catch (Exception exc)
+        {
+            return (_) => Result<V>.Fail(exc.ToString());
+        }
+    }
 }
--- test/ResultLib.Tests/SyncResultDemo.cs
+++ test/ResultLib.Tests/SyncResultDemo.cs
@@ -7,7 +7,7 @@ public class SyncResultDemo(ITestOutputHelper output)
     [Fact]
     public void MakeThreeSteps()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 15; i++)
         {
             var c = Demo();
             output.WriteLine(c.Error ?? $"{c.Value}");
@@ -16,28 +16,29 @@ public class SyncResultDemo(ITestOutputHelper output)
 
     private Result<C> Demo()
     {
-        var g = GetA;
-        var a = g.Curry(DateTime.Now);
-        
-        var f = GetC;
-        var c = f.Curry(DateTime.Now)!.Curry(a().Value);
-        return GetB(DateTime.Now).Transform(c);
+        var a = GetA;
+        var b = GetB;
+        var c = GetC;
+
+        var dt = DateTime.Now;
+
+        return a(dt).Apply(b(dt).CurryApply(c.Curry(dt)));
     }
 
     private Result<A> GetA(DateTime dt)
         => dt.Microsecond % 2 == 0
-        ? Result.Create(new A(dt.Microsecond))
-        : Result.Create<A>(error: $"no A for {dt.Microsecond}");
+        ? new A(dt.Microsecond)
+        : Result<A>.Fail($"no A for {dt.Microsecond}");
 
     private Result<B> GetB(DateTime dt)
-        => dt.Microsecond % 2 == 0
-        ? Result.Create(new B(dt.Microsecond))
-        : Result.Create<B>(error: $"no B for {dt.Microsecond}");
+        => dt.Microsecond % 3 == 0
+        ? new B(dt.Microsecond)
+        : Result<B>.Fail($"no B for {dt.Microsecond}");
 
     private Result<C> GetC(DateTime dt, A a, B b)
-        => dt.Microsecond % 2 == 0
-        ? Result.Create(new C(dt.Microsecond + a.a + b.b))
-        : Result.Create<C>(error: $"no C for {dt.Microsecond}");
+        => dt.Microsecond % 5 == 0
+        ? new C(dt.Microsecond + a.a + b.b)
+        : Result<C>.Fail(error: $"no C for {dt.Microsecond}");
 
     private record A(int a);
     private record B(int b);
