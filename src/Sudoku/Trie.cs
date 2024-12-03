9a152e116e5bf0b0bb3a7045570405f2c3eafee2
Illia Levandovskyi <lis2889@gmail.com>
2024-12-03 19:54:06 +0100

    Lexical prefix tree (Trie) demo is added

---
+++ src/Sudoku/Trie.cs
@@ -0,0 +1,91 @@
+﻿using System.Buffers;
+
+namespace Sudoku;
+
+/// <summary> aka Lexical prefix tree  </summary>
+public class Trie(char value)
+{
+    //public Trie? Parent { get; init;}
+    public Dictionary<char, Trie> Children { get; } = new();
+    public char Value { get; } = value;
+    public int Counter { get; set; } = 0;
+}
+
+public static class TrieHelpers
+{
+    private static readonly SearchValues<char> _wordEnd = SearchValues.Create(".,:;!?/ \'\"\t\n\r()[]{}");
+
+    public static int GetTotalWordsCount(Trie frequencyTree)
+    {
+        var total = 0;
+    
+        var layer = new Queue<Trie>();
+        layer.Enqueue(frequencyTree);
+        
+        while (layer.Any())
+        {
+            var current = layer.Dequeue();
+            total += current.Counter;
+        
+            foreach (var child in current.Children.Values)
+                layer.Enqueue(child);
+        }
+
+        return total;
+    }
+
+    public static int GetWordCount(Trie frequencyTree, string word)
+    {
+        var current = frequencyTree;
+        foreach (var ch in word)
+        {
+            if (!current.Children.TryGetValue(ch, out var child))
+                return 0;//the word is not in the tree
+            current = child;
+        }
+        return current.Counter;//if 0, the word is not in the tree
+    }
+
+    public static Trie BuildWordFrequencyTree(IEnumerable<string> source)
+    {
+        var root = new Trie(' ');
+        foreach (var s in source)
+        {
+            var word = new List<char>(30);
+            foreach (var ch in s)
+            {
+                if (_wordEnd.Contains(ch))
+                {
+                    PutWordInTree(root, word.ToArray());
+                    word.Clear();
+                    continue;
+                }
+                word.Add(ch);
+            }
+            if (word.Any())
+                PutWordInTree(root, word.ToArray());
+        }
+        return root;
+    }
+
+    private static void PutWordInTree(Trie root, char[] word)
+    {
+        var current = root;
+        for (int i = 0; i < word.Length - 1; i++)
+        {
+            if (!current.Children.TryGetValue(word[i], out var child))
+            {
+                child = new Trie(word[i]);
+                current.Children.Add(word[i], child);
+            }
+            current = child;
+        }
+
+        if (!current.Children.TryGetValue(word.Last(), out var wordEnd))
+        {
+            wordEnd = new Trie(word.Last());
+            current.Children.Add(word.Last(), wordEnd);
+        }
+        wordEnd.Counter++;
+    }
+}
---
+++ test/Sudoku.Tests/TrieHelpersTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using System.Diagnostics;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace Sudoku.Tests;
+
+[Trait("Category", "Unit")]
+public class TrieHelpersTests(ITestOutputHelper output)
+{
+    [Fact]
+    public void Build_GetTotalWords_PrintFrequencyTable()
+    {
+        const int size = 100_000;
+
+        var beforeWords = Process.GetCurrentProcess().PagedMemorySize64;
+        var words = Enumerable.Range(1, size).Select(n => GenerateWord(Random.Shared.Next(1, 10))).ToArray();
+        var afterWords = Process.GetCurrentProcess().PagedMemorySize64;
+        output.WriteLine($"Words consumed memory {(afterWords-beforeWords)/1024.0/1024:N2} MB");
+
+        var beforeTree = Process.GetCurrentProcess().PagedMemorySize64;
+        var frequencyTree = TrieHelpers.BuildWordFrequencyTree(words);
+        var afterTree = Process.GetCurrentProcess().PagedMemorySize64;
+        output.WriteLine($"Tree consumed memory {(afterTree-beforeTree)/1024.0/1024:N2} MB");
+
+        var beforeDictionary = Process.GetCurrentProcess().PagedMemorySize64;
+        var frequencyDictionary = words.GroupBy(w => w).ToDictionary(g => g.Key, g => g.Count());
+        var afterDictionary = Process.GetCurrentProcess().PagedMemorySize64;
+        output.WriteLine($"Dictionary consumed memory {(afterDictionary-beforeDictionary)/1024.0/1024:N2} MB");
+        output.WriteLine($"total words {frequencyDictionary.Values.Sum()}");
+
+        var totalWordsNumber = TrieHelpers.GetTotalWordsCount(frequencyTree);
+
+        totalWordsNumber.Should().Be(size);
+        foreach (var word in words.Distinct().OrderByDescending(w => w.Length))
+        {
+            var count = TrieHelpers.GetWordCount(frequencyTree, word);
+            count.Should().Be(frequencyDictionary[word]);
+            if (count >= 2)
+                output.WriteLine($"'{word}' is met {count} times");
+        }
+    }
+
+    private static readonly char[] Alphabet = "abcdefghijklmnopqrstuvwxyz".OrderBy(ch => ch).ToArray();
+
+    private static string GenerateWord(int length)
+    {
+        var sb = new StringBuilder(length);
+        
+        for (int i = 0; i < length; i++)
+            sb.Append(Alphabet[Random.Shared.Next(0, Alphabet.Length)]);
+
+        return sb.ToString();
+    }
+}
