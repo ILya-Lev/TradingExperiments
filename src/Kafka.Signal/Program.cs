a9bead00dd8a1940c2a9099b7aec66044ee56181
Illia Levandovskyi <lis2889@gmail.com>
2024-12-25 22:29:12 +0100

    kafka producer and consumer PoC

---
+++ src/Kafka.Signal/Program.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+
+//see https://github.com/confluentinc/confluent-kafka-dotnet/
+Console.WriteLine("Hello, from kafka signal app - press enter to quit!");
+
+const string topic = "kinaction_helloworld";
+var config = new ProducerConfig() { BootstrapServers = "localhost:9094", };
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+try
+{
+    for (var i = 0; i < 10; i++)
+    {
+        var message = new Message<Null, string>() { Value = $"{DateTimeOffset.Now:R} {i}" };
+        Console.WriteLine($"Sending message: {message.Value}");
+
+        var deliveryResult = await producer.ProduceAsync(topic, message);
+        Console.WriteLine($"Delivered {deliveryResult.Value} to {deliveryResult.TopicPartitionOffset}");
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+}
+catch (Exception exc)
+{
+    Console.WriteLine(exc);
+}
+Console.ReadLine();
---
+++ src/Kafka.Slot/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+//see https://github.com/confluentinc/confluent-kafka-dotnet/
+Console.WriteLine("Hello from kafka slot - press enter to quit!");
+
+const string topic = "kinaction_helloworld";
+var config = new ConsumerConfig()
+{
+    GroupId = "aaa",
+    BootstrapServers = "localhost:9094",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+consumer.Subscribe(topic);
+
+while (true)
+{
+    try
+    {
+        var consumeResult = consumer.Consume();
+        Console.WriteLine($"consumed message {consumeResult.Message.Value} from {consumeResult.TopicPartitionOffset}");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"error consuming message: {exc.Message}");
+    }
+}
+
+Console.ReadLine();
