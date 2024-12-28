d282134b2242fd77035b7bc12acdd36e36f5c661
Illia Levandovskyi <lis2889@gmail.com>
2024-12-28 12:36:03 +0100

    some problem set solutions for udemy traning by Cameron Connell

---
+++ src/BlackScholesMerton/CameronConnellUdemy/PresentValueCalculator.cs
@@ -0,0 +1,39 @@
+﻿namespace BlackScholesMerton.CameronConnellUdemy;
+
+internal class PresentValueCalculator
+{
+    public static decimal GetPresentValue(
+        Dictionary<decimal, (decimal r, decimal c)> paymentsAndInterestByPeriod,
+        int compounding) => paymentsAndInterestByPeriod
+        .Sum(pair =>
+        {
+            var time = pair.Key;
+            var r = pair.Value.r;
+            var c = pair.Value.c;
+
+            var discount = 1 / Math.Pow(1 + (double)r / compounding, compounding * (double)time);
+
+            return c * (decimal)discount;
+        });
+
+    public static decimal GetBondPrice(
+        decimal faceValue,
+        decimal couponRate,
+        decimal flatInterestRate,
+        int compounding,
+        int maturity)
+    {
+        var discount = 1m;
+        var aggregatedDiscount = 0m;
+        for (int payment = 1; payment < compounding * maturity; payment++)
+        {
+            discount /= (1 + flatInterestRate / compounding);
+            aggregatedDiscount += discount;
+        }
+
+        discount /= (1 + flatInterestRate / compounding);
+        return faceValue
+               * (aggregatedDiscount * couponRate / compounding
+                  + (1 + couponRate / compounding) * discount);
+    }
+}
---
+++ test/BlackScholesMerton.Tests/CameronConnellUdemy/PresentValueCalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using BlackScholesMerton.CameronConnellUdemy;
+using FluentAssertions;
+using Xunit.Sdk;
+
+namespace BlackScholesMerton.Tests.CameronConnellUdemy;
+
+[Trait("Category", "Unit")]
+public class PresentValueCalculatorTests
+{
+    [Fact]
+    public void GetPresentValue_ProblemSet1Task2_Observe()
+    {
+        var compounding = 2;
+        var paymentsAndInterestByPeriod = new Dictionary<decimal, (decimal r, decimal c)>()
+        {
+            [0.5m] = (0.02m, 100),
+            [1m] = (0.03m, 100),
+            [2m] = (0.035m, 200),
+            [3m] = (0.042m, 200),
+        };
+        
+        var presentValue = PresentValueCalculator.GetPresentValue(paymentsAndInterestByPeriod, compounding);
+
+        presentValue.Should().BeApproximately(559.2209565m, 1e-7m);
+    }
+
+    [Fact]
+    public void GetBondPrice_ProblemSet1Task3_Observe()
+    {
+        var price = PresentValueCalculator
+            .GetBondPrice(10, 0.06m, 0.04m, 2, 2);
+
+        price.Should().BeApproximately(10.3807729m, 1e-7m);
+    }
+}
