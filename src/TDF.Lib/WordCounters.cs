8b6b6c13a9b005e96fbdb1a00a0f47d4b4efb8fb
Illia Levandovskyi <lis2889@gmail.com>
2025-03-09 01:03:56 +0100

    added parallel words counter - benchmark (linq based, serial), plinq and parallel persions. TDF is pending, as wel as tests and agent based impl

---
+++ src/TDF.Lib/WordCounters.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Concurrent;
+
+namespace TDF.Lib;
+
+public interface IWordCounter
+{
+    Task<IReadOnlyDictionary<string, int>> GetWordsStatistic(IEnumerable<string> fileNames);
+}
+
+public class WordCounterLinqAggregate : IWordCounter
+{
+    public Task<IReadOnlyDictionary<string, int>> GetWordsStatistic(IEnumerable<string> fileNames) 
+        => Task.FromResult(DoGetWordsStatistic(fileNames));
+
+    private IReadOnlyDictionary<string, int> DoGetWordsStatistic(IEnumerable<string> fileNames) => fileNames
+        .Aggregate(
+            seed: new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase),
+            func: (accumulator, fileName) =>
+            {
+                using var fileStream = File.OpenRead(fileName);
+                using var streamReader = new StreamReader(fileStream);
+
+                for (var line = streamReader.ReadLine(); line is not null; line = streamReader.ReadLine())
+                {
+                    foreach (var word in line.Split(' '))
+                    {
+                        var wordChars = word.ToCharArray().Where(char.IsLetterOrDigit).ToArray();
+                        if (wordChars.Length == 0)
+                            continue;
+
+                        var cleanedWord = new string(wordChars);
+                        if (!accumulator.TryAdd(cleanedWord, 1))
+                            accumulator[cleanedWord]++;
+                    }
+                }
+
+                return accumulator;
+            }
+        );
+}
+
+public class WordCounterPlinqAggregate : IWordCounter
+{
+    public Task<IReadOnlyDictionary<string, int>> GetWordsStatistic(IEnumerable<string> fileNames) 
+        => Task.FromResult(DoGetWordsStatistic(fileNames));
+
+    private IReadOnlyDictionary<string, int> DoGetWordsStatistic(IEnumerable<string> fileNames) => fileNames
+        .AsParallel()
+        .WithDegreeOfParallelism(degreeOfParallelism: Environment.ProcessorCount - 1)
+        .WithExecutionMode(executionMode: ParallelExecutionMode.ForceParallelism)
+        .WithMergeOptions(mergeOptions: ParallelMergeOptions.NotBuffered)
+        .Aggregate(
+            seedFactory: () => new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase),
+            updateAccumulatorFunc: (accumulator, fileName) =>
+            {
+                using var fileStream = File.OpenRead(fileName);
+                using var streamReader = new StreamReader(fileStream);
+
+                for (var line = streamReader.ReadLine(); line is not null; line = streamReader.ReadLine())
+                {
+                    foreach (var word in line.Split(' '))
+                    {
+                        var wordChars = word.ToCharArray().Where(char.IsLetterOrDigit).ToArray();
+                        if (wordChars.Length == 0)
+                            continue;
+
+                        var cleanedWord = new string(wordChars);
+                        if (!accumulator.TryAdd(cleanedWord, 1))
+                            accumulator[cleanedWord]++;
+                    }
+                }
+
+                return accumulator;
+            },
+            combineAccumulatorsFunc: (lhs, rhs) =>
+            {
+                foreach(var (word, counter) in rhs)
+                    if (!lhs.TryAdd(word, counter))
+                        lhs[word] += counter;
+                
+                return lhs;
+            },
+            resultSelector: accumulator => accumulator
+        );
+}
+
+public class WordCounterParallelAsyncEnumerable : IWordCounter
+{
+    public async Task<IReadOnlyDictionary<string, int>> GetWordsStatistic(IEnumerable<string> fileNames)
+    {
+        var accumulator = new ConcurrentDictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+        await Parallel.ForEachAsync(
+            source: ReadLines(fileNames),
+            parallelOptions: new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount - 1,
+            },
+            body: (line, ct) =>
+            {
+                foreach (var word in line?.Split(' ') ?? [])
+                {
+                    var wordChars = word.ToCharArray().Where(char.IsLetterOrDigit).ToArray();
+                    if (wordChars.Length == 0)
+                        continue;
+
+                    var cleanedWord = new string(wordChars);
+                    accumulator.AddOrUpdate(cleanedWord, w => 1, (w, counter) => counter + 1);
+                }
+                return ValueTask.CompletedTask;
+            });
+
+        return accumulator;
+    }
+
+    private static async IAsyncEnumerable<string?> ReadLines(IEnumerable<string> fileNames)
+    {
+        foreach (var fileName in fileNames)
+        {
+            await using var fileStream = File.OpenRead(fileName);
+            using var streamReader = new StreamReader(fileStream);
+
+            var line = await streamReader.ReadLineAsync();
+            while (line is not null)
+            {
+                yield return line;
+                line = await streamReader.ReadLineAsync();
+            }
+        }
+    }
+}
+
---
+++ test/TDF.Tests/WordCounterTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+using TDF.Lib;
+using Xunit.Abstractions;
+
+namespace TDF.Tests;
+
+public class WordCounterTests(ITestOutputHelper output)
+{
+    [Fact]
+    public async Task GetWordsStatistic_MbFiles_DifferentApproachesMatch()
+    {
+        var folder = @"";
+        var fullNames = Directory.EnumerateFiles(folder, "*.*", SearchOption.AllDirectories).ToArray();
+
+        var sw = Stopwatch.StartNew();
+        var benchmark = await new WordCounterLinqAggregate().GetWordsStatistic(fullNames);
+        sw.Stop();
+        var elapsedBenchmark = sw.Elapsed;
+        
+        sw = Stopwatch.StartNew();
+        var plinqResult = await new WordCounterPlinqAggregate().GetWordsStatistic(fullNames);
+        sw.Stop();
+        var elapsedPlinq = sw.Elapsed;
+        
+        sw = Stopwatch.StartNew();
+        var asyncEnumerable = await new WordCounterParallelAsyncEnumerable().GetWordsStatistic(fullNames);
+        sw.Stop();
+        var elapsedAsyncEnumerable = sw.Elapsed;
+
+        output.WriteLine($"benchmark (linq, single threaded) took {elapsedBenchmark}");
+        output.WriteLine($"plinq took {elapsedPlinq}");
+        output.WriteLine($"parallel and async enumerable took {elapsedAsyncEnumerable}");
+
+        AssertMatchingStatistic(benchmark, plinqResult);
+        AssertMatchingStatistic(benchmark, asyncEnumerable);
+    }
+
+    private static void AssertMatchingStatistic(IReadOnlyDictionary<string, int> lhs
+        , IReadOnlyDictionary<string, int> rhs)
+    {
+        var mismatch = new List<string>();
+        foreach (var (word, count) in lhs)
+        {
+            if (rhs.TryGetValue(word, out var rhsCount))
+                count.Should().Be(rhsCount);
+            else
+                mismatch.Add(word);
+        }
+
+        mismatch.Should().BeEmpty();
+    }
+}
