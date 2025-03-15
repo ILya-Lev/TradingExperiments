f72c06427a8958aba74c9486a8643b604a8d4f2e
Illia Levandovskyi <lis2889@gmail.com>
2025-03-15 13:15:10 +0100

    concurrent impl for quick sort is added

---
+++ src/TDF.Lib/QuickSorter.cs
@@ -0,0 +1,199 @@
+﻿namespace TDF.Lib;
+
+public interface IQuickSorter<T>
+{
+    Task<T[]> Sort(T[] array);
+}
+
+public class QuickSorter<T>(Func<T, T, bool> isBefore) : IQuickSorter<T>
+{
+    public Task<T[]> Sort(T[] array)
+    {
+        if (IsSorted(array))
+            return Task.FromResult(array);
+
+        var states = new Stack<(int start, int end)>();
+        states.Push((0, array.Length - 1));
+
+        while (states.Any())
+        {
+            var (start, end) = states.Pop();
+            var middle = PutOneItemInItsFinalPosition(array, start, end);
+
+            if (middle - 1 > start) states.Push((start, middle - 1));
+            if (end > middle + 1) states.Push((middle + 1, end));
+        }
+
+        return Task.FromResult(array);
+    }
+
+    private bool IsSorted(T[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (isBefore(array[i], array[i - 1]))
+                return false;
+        }
+        return true;
+    }
+
+    private int PutOneItemInItsFinalPosition(T[] array, int start, int end)
+    {
+        var seed = Random.Shared.Next(start, end + 1);
+        Swap(array, start, seed);
+
+        var head = start;
+        var tail = end;
+        while (head < tail)
+        {
+            if (isBefore(array[head], array[head + 1]))
+            {
+                Swap(array, head + 1, tail);
+                tail--;
+            }
+            else
+            {
+                Swap(array, head, head + 1);
+                head++;
+            }
+        }
+
+        return head;
+    }
+
+    private static void Swap(T[] array, int lhs, int rhs) => (array[lhs], array[rhs]) = (array[rhs], array[lhs]);
+}
+
+public class QuickSorterAsync<T>(Func<T, T, bool> isBefore) : IQuickSorter<T>
+{
+    public async Task<T[]> Sort(T[] array)
+    {
+        if (IsSorted(array))
+            return array;
+
+        return await DoSort(array, 0, array.Length - 1, Environment.ProcessorCount);
+    }
+    private async Task<T[]> DoSort(T[] array, int start, int end, int threads)
+    {
+        await Task.Yield();
+
+        var middle = PutOneItemInItsFinalPosition(array, start, end);
+
+        if (threads > 0)
+        {
+            var tasks = new List<Task<T[]>>();
+            if (middle - 1 > start) tasks.Add(DoSort(array, start, middle - 1, threads - 1));
+            if (end > middle + 1) tasks.Add(DoSort(array, middle + 1, end, threads - 2));
+            await Task.WhenAll(tasks);
+            return array;
+        }
+
+        if (middle - 1 > start) await DoSort(array, start, middle - 1, threads - 1);
+        if (end > middle + 1) await DoSort(array, middle + 1, end, threads - 2);
+
+        return array;
+    }
+
+    private bool IsSorted(T[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (isBefore(array[i], array[i - 1]))
+                return false;
+        }
+        return true;
+    }
+
+    private int PutOneItemInItsFinalPosition(T[] array, int start, int end)
+    {
+        var seed = Random.Shared.Next(start, end + 1);
+        Swap(array, start, seed);
+
+        var head = start;
+        var tail = end;
+        while (head < tail)
+        {
+            if (isBefore(array[head], array[head + 1]))
+            {
+                Swap(array, head + 1, tail);
+                tail--;
+            }
+            else
+            {
+                Swap(array, head, head + 1);
+                head++;
+            }
+        }
+
+        return head;
+    }
+
+    private static void Swap(T[] array, int lhs, int rhs) => (array[lhs], array[rhs]) = (array[rhs], array[lhs]);
+}
+
+public class QuickSorterParallel<T>(Func<T, T, bool> isBefore) : IQuickSorter<T>
+{
+    public async Task<T[]> Sort(T[] array)
+    {
+        await Task.Yield();
+
+        if (!IsSorted(array))
+            DoSort(array, 0, array.Length - 1, Environment.ProcessorCount);
+
+        return array;
+    }
+    private void DoSort(T[] array, int start, int end, int threads)
+    {
+        var middle = PutOneItemInItsFinalPosition(array, start, end);
+
+        if (threads > 2)
+        {
+            var funcs = new List<Action>();
+            if (middle - 1 > start) funcs.Add(() => DoSort(array, start, middle - 1, threads - 1));
+            if (end > middle + 1) funcs.Add(() => DoSort(array, middle + 1, end, threads - 2));
+            Parallel.Invoke(new ParallelOptions() { MaxDegreeOfParallelism = threads }, funcs.ToArray());
+        }
+        else
+        {
+            if (middle - 1 > start) DoSort(array, start, middle - 1, threads - 1);
+            if (end > middle + 1) DoSort(array, middle + 1, end, threads - 2);
+        }
+    }
+
+    private bool IsSorted(T[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (isBefore(array[i], array[i - 1]))
+                return false;
+        }
+        return true;
+    }
+
+    private int PutOneItemInItsFinalPosition(T[] array, int start, int end)
+    {
+        var seed = Random.Shared.Next(start, end + 1);
+        Swap(array, start, seed);
+
+        var head = start;
+        var tail = end;
+        while (head < tail)
+        {
+            if (isBefore(array[head], array[head + 1]))
+            {
+                Swap(array, head + 1, tail);
+                tail--;
+            }
+            else
+            {
+                Swap(array, head, head + 1);
+                head++;
+            }
+        }
+
+        return head;
+    }
+
+    private static void Swap(T[] array, int lhs, int rhs) => (array[lhs], array[rhs]) = (array[rhs], array[lhs]);
+}
+
---
+++ test/TDF.Tests/QuickSorterTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using TDF.Lib;
+
+namespace TDF.Tests;
+
+public class QuickSorterTests
+{
+    private static readonly Func<int, int, bool> _isBefore = (a, b) => a < b;
+    [Fact]
+    public async Task Sort_DescendingShort_Ascending()
+    {
+        var source = new[] {1, 4, 5, 6, 8, 9, 17, 11, 8, 12}
+            .OrderByDescending(n => n)
+            .ToArray();
+
+        var sorter = new QuickSorterAsync<int>(_isBefore);
+
+        var sorted = await sorter.Sort(source);
+
+        sorted.Should().BeInAscendingOrder();
+    }
+
+    [Fact]
+    public async Task Sort_Random_Ascending()
+    {
+        var source = Enumerable.Range(1, 10_000_000)
+            .Select(n => Random.Shared.Next(n + 10))
+            .ToArray();
+
+        var sorter = new QuickSorterParallel<int>(_isBefore);
+
+        var sorted = await sorter.Sort(source);
+
+        sorted.Should().BeInAscendingOrder();
+    }
+}
