ba3fabd1516e5137f0e40539c491fc07bece0d71
Illia Levandovskyi <lis2889@gmail.com>
2025-03-22 14:22:31 +0100

    simple parallel operation - ordered and unordered comparison - too simple - no effect

--- test/TDF.Tests/AsyncStreamDemoTests.cs
+++ test/TDF.Tests/AsyncStreamDemoTests.cs
@@ -43,4 +43,43 @@ public class AsyncStreamDemoTests(ITestOutputHelper output)
         parallel.Should().BeApproximately(sequential, 1e-8);
         plinq.Should().BeApproximately(sequential, 1e-8);
     }
+
+    [Fact]
+    public void Multiply_WithAndWithoutAsOrdered_Compare()
+    {
+        var numbers = Enumerable.Range(1, 1_000_000_000).ToArray();
+
+        var sw = Stopwatch.StartNew();
+        var sequential = numbers.Select(n => n * 2).ToArray();
+        sw.Stop();
+        var elapsedSequentially = sw.Elapsed;
+
+        sw = Stopwatch.StartNew();
+        var unordered = numbers
+            .AsParallel().AsUnordered().WithDegreeOfParallelism(Environment.ProcessorCount-1)
+            .Select(n => n * 2).ToArray();
+        sw.Stop();
+        var elapsedUnordered = sw.Elapsed;
+
+        sw = Stopwatch.StartNew();
+        var ordered = numbers
+            .AsParallel().AsOrdered().WithDegreeOfParallelism(Environment.ProcessorCount-1)
+            .Select(n => n * 2).ToArray();
+        sw.Stop();
+        var elapsedOrdered = sw.Elapsed;
+
+        output.WriteLine($"sequential run took {elapsedSequentially}");
+        output.WriteLine($"plinq unordered run took {elapsedUnordered}");
+        output.WriteLine($"plinq ordered run took {elapsedOrdered}");
+
+        //up to this place ~ half of the method run time 
+        for (int i = 0; i < sequential.Length; i++)
+        {
+            if (unordered[i] != sequential[i])
+                throw new Exception($"{i} element unordered {unordered[i]} mismatch sequential {sequential[i]}");
+
+            if (ordered[i] != sequential[i])
+                throw new Exception($"{i} element ordered {ordered[i]} mismatch sequential {sequential[i]}");
+        }
+    }
 }
