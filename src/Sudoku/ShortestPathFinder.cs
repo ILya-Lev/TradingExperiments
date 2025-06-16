21e69bbc8e82b3eb6861776a50abf86f2492f746
Illia Levandovskyi <lis2889@gmail.com>
2025-06-17 00:22:04 +0200

    Dijkstra impl

---
+++ src/Sudoku/ShortestPathFinder.cs
@@ -0,0 +1,65 @@
+﻿namespace Sudoku;
+
+public static class ShortestPathFinder
+{
+    public record Node(string Name)
+    {
+        public Dictionary<Node, int> Next { get; } = new();
+    }
+
+    public static string GetShortestPath(Node start, Node end)
+    {
+        var hops = BuildHops(start, end);
+        if (hops.Count == 0) return string.Empty;
+
+        var length = 0;
+        var path = new List<Node> { end };
+        var current = end;
+        while (current != start)
+        {
+            var previous = hops[current];
+            length += previous.Next[current];
+            path.Add(previous);
+            current = previous;
+        }
+
+        path.Reverse();
+        return string.Join(" -> ", path.Select(n => n.Name)) + $" total length {length}";
+    }
+
+    private static IReadOnlyDictionary<Node, Node> BuildHops(Node start, Node end)
+    {
+        //to a given key what node was the one with the shortest distance
+        var hops = new Dictionary<Node, Node> { [start] = start };
+
+        //for a given key what is the shortest distance from the start
+        var distances = new Dictionary<Node, int> { [start] = 0 };
+
+        //take the next node closest to the current set of observed nodes
+        var frontier = new PriorityQueue<Node, int>();
+        frontier.Enqueue(start, 0);
+
+        while (frontier.Count > 0)
+        {
+            var current = frontier.Dequeue();
+
+            var currentDistance = distances[current];
+            foreach (var (nextNode, nextDistance) in current.Next)
+            {
+                var oldDistance = distances.GetValueOrDefault(nextNode, int.MaxValue);
+                var newDistance = currentDistance + nextDistance;
+
+                if (oldDistance > newDistance)
+                {
+                    frontier.Enqueue(nextNode, newDistance);
+                    hops[nextNode] = current;
+                    distances[nextNode] = newDistance;
+                    //do not try to short-circuit - thus we'll contain the first path reaching the target node
+                    //not the shortest path to the target node.
+                }
+            }
+        }
+
+        return hops.ContainsKey(end) ? hops : new Dictionary<Node, Node>();
+    }
+}
---
+++ test/Sudoku.Tests/ShortestPathFinderTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+
+namespace Sudoku.Tests;
+
+[Trait("Category", "Unit")]
+public class ShortestPathFinderTests
+{
+    [Fact]
+    public void GetShortestPath_5Nodes_ShortestPathMaxHops()
+    {
+        var a = new ShortestPathFinder.Node("A");
+        var b = new ShortestPathFinder.Node("B");
+        var c = new ShortestPathFinder.Node("C");
+        var d = new ShortestPathFinder.Node("D");
+        var e = new ShortestPathFinder.Node("E");
+
+        a.Next[b] = 1;
+        a.Next[c] = 2;
+        
+        b.Next[d] = 4;
+        b.Next[e] = 5;
+
+        c.Next[d] = 1;
+        c.Next[e] = 4;
+
+        d.Next[e] = 1;
+
+        var report = ShortestPathFinder.GetShortestPath(a, e);
+
+        report.Should().Be("A -> C -> D -> E total length 4");
+    }
+}
