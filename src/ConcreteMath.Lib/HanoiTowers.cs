a41e05d33da89d8206c7ae6536857e10ef499992
Illia Levandovskyi <lis2889@gmail.com>
2026-01-26 00:05:47 +0100

    hanoi towers recursive solution

---
+++ src/ConcreteMath.Lib/HanoiTowers.cs
@@ -0,0 +1,21 @@
+﻿namespace ConcreteMath.Lib;
+
+public static class HanoiTowers
+{
+    public readonly record struct Step(int From, int To, int BlockSize);
+
+    public static IEnumerable<Step> Solve(int blocks)
+    {
+        return Solve(blocks, 1, 3, 2);
+    }
+
+    private static IEnumerable<Step> Solve(int blocks, int from, int to, int spare)
+    {
+        if (blocks < 1) return [];
+        if (blocks == 1) return [new(from, to, blocks)];
+
+        return Solve(blocks - 1, from, spare, to)
+            .Concat([new(from, to, blocks)])
+            .Concat(Solve(blocks - 1, spare, to, from));
+    }
+}
---
+++ test/ConcreteMath.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using ConcreteMath.Lib;
+using FluentAssertions;
+
+namespace ConcreteMath.Tests;
+
+public class HanoiTowersTests(ITestOutputHelper outputHelper)
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(2, 3)]
+    [InlineData(3, 7)]
+    [InlineData(4, 15)]
+    [InlineData(5, 31)]
+    [InlineData(6, 63)]
+    [InlineData(7, 127)]
+    [InlineData(8, 255)]
+    [InlineData(9, 511)]
+    public void Solve_N_MatchExpectedStepsNumber(int blocks, int expectedCount)
+    {
+        var steps = HanoiTowers.Solve(blocks).ToArray();
+
+        steps.Should().HaveCount(expectedCount);
+        for (var i = 0; i < steps.Length; i++)
+        {
+            var s = steps[i];
+            outputHelper.WriteLine($"{i + 1}: {s.From} -> {s.To} block {s.BlockSize}");
+        }
+    }
+}
