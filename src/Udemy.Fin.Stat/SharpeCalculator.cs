df3e0964568f0c03e9fae6850b65e9409a25f13d
Illia Levandovskyi <lis2889@gmail.com>
2026-03-03 20:36:47 +0100

    sharpe calculator is added (not sure if correct)

---
+++ src/Udemy.Fin.Stat/SharpeCalculator.cs
@@ -0,0 +1,36 @@
+﻿using MathNet.Numerics.Statistics;
+
+namespace Udemy.Fin.Stat;
+
+public static class SharpeCalculator
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="prices"></param>
+    /// <param name="aggregationWindow"></param>
+    /// <param name="riskFreeReturn">here is a default constant = 3%; but should be time-series per quarter or so</param>
+    /// <returns></returns>
+    public static IEnumerable<decimal> CalculateDailySharpe(IEnumerable<decimal> prices, 
+        int aggregationWindow, 
+        decimal riskFreeReturn = 0.03m)
+    {
+        var buffer = new Queue<decimal>();
+        using var iterator = prices.GetEnumerator();
+        
+        while (buffer.Count < aggregationWindow && iterator.MoveNext())
+            buffer.Enqueue(iterator.Current);
+
+        while (buffer.Count == aggregationWindow)
+        {
+            var returns = ReturnsCalculator.CalculateContinuouslyCompoundedDailyReturns(buffer);
+            var stat = new DescriptiveStatistics(returns.Select(r => (double)r));
+
+            yield return ((decimal)stat.Mean - riskFreeReturn) / (decimal)stat.StandardDeviation;
+
+            buffer.Dequeue();
+            if (iterator.MoveNext())
+                buffer.Enqueue(iterator.Current);
+        }
+    }
+}
--- test/Udemy.Fin.Stat.Tests/ReturnsCalculatorTests.cs
+++ test/Udemy.Fin.Stat.Tests/ReturnsCalculatorTests.cs
@@ -9,11 +9,11 @@ public class ReturnsCalculatorTests(ITestOutputHelper output)
     public void CalculateVariousReturns_SamplePrices_ObserveValue()
     {
         var prices = new[] { 1447m, 1465, 1471, 1453, 1468, 1477, 1482, 1467, 1484, 1474 };
-        
+
         var dailyGross = CalculateDailyGrossReturns(prices);
         var dailyNet = CalculateDailyNetReturns(prices);
         var dailyCompounded = CalculateContinuouslyCompoundedDailyReturns(prices);
-        
+
         var weeklyGross = CalculateAggregatedGrossReturns(prices, 5);
         var weeklyNet = CalculateAggregatedNetReturns(prices, 5);
         var weeklyCompounded = CalculateAggregatedContinuouslyCompoundedReturns(prices, 5);
@@ -22,7 +22,7 @@ public class ReturnsCalculatorTests(ITestOutputHelper output)
         output.WriteLine($"gross: {string.Join("; ", dailyGross.Select(g => g.ToString("N4")))}");
         output.WriteLine($"net:   {string.Join("; ", dailyNet.Select(r => $"{r.ToBp()} bp"))}");
         output.WriteLine($"comp:  {string.Join("; ", dailyCompounded.Select(r => $"{r.ToBp()} bp"))}");
-        
+
         output.WriteLine($"weekly");
         output.WriteLine($"gross: {string.Join("; ", weeklyGross.Select(g => g.ToString("N4")))}");
         output.WriteLine($"net:   {string.Join("; ", weeklyNet.Select(r => $"{r.ToBp()} bp"))}");
@@ -32,7 +32,7 @@ public class ReturnsCalculatorTests(ITestOutputHelper output)
     [Fact]
     public void RestoreBasedOnNetReturn_Observe()
     {
-        var netReturns = new decimal[] { -1.4m,1.3m,0.8m,-0.9m,1.9m,0.7m,1.1m,-0.7m,1.1m,-1.2m }
+        var netReturns = new decimal[] { -1.4m, 1.3m, 0.8m, -0.9m, 1.9m, 0.7m, 1.1m, -0.7m, 1.1m, -1.2m }
             .Select(r => r / 100/*convert percents into decimal fraction*/)
             .ToArray();
 
@@ -63,24 +63,24 @@ public class ReturnsCalculatorTests(ITestOutputHelper output)
         data = data.Zip(closeDailyNetReturns, (d, n) => d with { DailyNet = n.ToBp() }).ToArray();
         data = data.Zip(closeContinuousReturns, (d, c) => d with { ContinuouslyCompound = c.ToBp() }).ToArray();
 
-        foreach(var item in data.Take(10))
+        foreach (var item in data.Take(10))
         {
             output.WriteLine($"{item.Date} {item.ContinuouslyCompound} bp {item.DailyNet} bp");
         }
 
         var savingTasks = new[]
         {
-            Plot("SnP500 close prices.png", data, i => i.Price),
-            Plot("SnP500 continuous returns.png", data, i => i.ContinuouslyCompound),
-            Plot("SnP500 daily net returns.png", data, i => i.DailyNet),
-            Plot("SnP500 continuous returns the 90th.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 1990 and < 2000),
-            Plot("SnP500 continuous returns dot com.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 2000 and < 2002),
-            Plot("SnP500 continuous returns the 2001-2007.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 2002 and < 2008),
-            Plot("SnP500 continuous returns big crisis 2008.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 2008 and < 2010),
+            Plot("SnP500 close prices.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.Price),
+            Plot("SnP500 continuous returns.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound),
+            Plot("SnP500 daily net returns.png", data, i => i.Date.ToDateTime(TimeOnly.MinValue),i => i.DailyNet),
+            Plot("SnP500 continuous returns the 90th.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 1990 and < 2000),
+            Plot("SnP500 continuous returns dot com.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 2000 and < 2002),
+            Plot("SnP500 continuous returns the 2001-2007.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 2002 and < 2008),
+            Plot("SnP500 continuous returns big crisis 2008.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 2008 and < 2010),
         };
         await Task.WhenAll(savingTasks);
     }
@@ -103,27 +103,28 @@ public class ReturnsCalculatorTests(ITestOutputHelper output)
         data = data.Zip(closeDailyNetReturns, (d, n) => d with { DailyNet = n.ToBp() }).ToArray();
         data = data.Zip(closeContinuousReturns, (d, c) => d with { ContinuouslyCompound = c.ToBp() }).ToArray();
 
-        foreach(var item in data.Take(10))
+        foreach (var item in data.Take(10))
         {
             output.WriteLine($"{item.Date} {item.ContinuouslyCompound} bp {item.DailyNet} bp");
         }
 
         var savingTasks = new[]
         {
-            Plot("Russell 2000 close prices.png", data, i => i.Price),
-            Plot("Russell 2000 continuous returns.png", data, i => i.ContinuouslyCompound),
-            Plot("Russell 2000 daily net returns.png", data, i => i.DailyNet),
-            Plot("Russell 2000 continuous returns the 90th.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 1990 and < 2000),
-            Plot("Russell 2000 continuous returns dot com.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 2000 and < 2002),
-            Plot("Russell 2000 continuous returns the 2001-2007.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 2002 and < 2008),
-            Plot("Russell 2000 continuous returns big crisis 2008.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 2008 and < 2010),
+            Plot("Russell 2000 close prices.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.Price),
+            Plot("Russell 2000 continuous returns.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound),
+            Plot("Russell 2000 daily net returns.png", data, i => i.Date.ToDateTime(TimeOnly.MinValue),i => i.DailyNet),
+            Plot("Russell 2000 continuous returns the 90th.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 1990 and < 2000),
+            Plot("Russell 2000 continuous returns dot com.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 2000 and < 2002),
+            Plot("Russell 2000 continuous returns the 2001-2007.png", data, i => i.Date.ToDateTime(TimeOnly.MinValue),i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 2002 and < 2008),
+            Plot("Russell 2000 continuous returns big crisis 2008.png", data, i => i.Date.ToDateTime(TimeOnly.MinValue),i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 2008 and < 2010),
         };
         await Task.WhenAll(savingTasks);
     }
+
     [Fact]
     public async Task EuroStoxx50_ContinuouslyCompounded_DailyNetReturns_Plot()
     {
@@ -142,43 +143,66 @@ public class ReturnsCalculatorTests(ITestOutputHelper output)
         data = data.Zip(closeDailyNetReturns, (d, n) => d with { DailyNet = n.ToBp() }).ToArray();
         data = data.Zip(closeContinuousReturns, (d, c) => d with { ContinuouslyCompound = c.ToBp() }).ToArray();
 
-        foreach(var item in data.Take(10))
+        foreach (var item in data.Take(10))
         {
             output.WriteLine($"{item.Date} {item.ContinuouslyCompound} bp {item.DailyNet} bp");
         }
 
         var savingTasks = new[]
         {
-            Plot("EuroStoxx50 close prices.png", data, i => i.Price),
-            Plot("EuroStoxx50 continuous returns.png", data, i => i.ContinuouslyCompound),
-            Plot("EuroStoxx50 daily net returns.png", data, i => i.DailyNet),
-            Plot("EuroStoxx50 continuous returns the 90th.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 1990 and < 2000),
-            Plot("EuroStoxx50 continuous returns dot com.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 2000 and < 2002),
-            Plot("EuroStoxx50 continuous returns the 2001-2007.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 2002 and < 2008),
-            Plot("EuroStoxx50 continuous returns big crisis 2008.png", data, i => i.ContinuouslyCompound,
-                d => d.Year is >= 2008 and < 2010),
+            Plot("EuroStoxx50 close prices.png", data, i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.Price),
+            Plot("EuroStoxx50 continuous returns.png", data, i => i.Date.ToDateTime(TimeOnly.MinValue),i => i.ContinuouslyCompound),
+            Plot("EuroStoxx50 daily net returns.png", data, i => i.Date.ToDateTime(TimeOnly.MinValue),i => i.DailyNet),
+            Plot("EuroStoxx50 continuous returns the 90th.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 1990 and < 2000),
+            Plot("EuroStoxx50 continuous returns dot com.png", data, i => i.Date.ToDateTime(TimeOnly.MinValue),i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 2000 and < 2002),
+            Plot("EuroStoxx50 continuous returns the 2001-2007.png", data,i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 2002 and < 2008),
+            Plot("EuroStoxx50 continuous returns big crisis 2008.png", data, i => i.Date.ToDateTime(TimeOnly.MinValue),i => i.ContinuouslyCompound,
+                i => i.Date.Year is >= 2008 and < 2010),
         };
         await Task.WhenAll(savingTasks);
     }
 
-    private static async Task Plot(string name,
-        PricesWithReturns[] data,
-        Func<PricesWithReturns, decimal> projection, 
-        Func<DateOnly, bool>? filter = null)
+    [Theory]
+    [InlineData("GSPC")]
+    [InlineData("RUT")]
+    public async Task Sharpe_ForIndexes_Plot(string fileName)
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "TestData", $"{fileName}.parquet");
+        var index = await DataLoader.LoadParquet<ExIndex>(path).ToArrayAsync();
+
+        var yearlySharpe = SharpeCalculator.CalculateDailySharpe(index.Select(i => i.Close), 250);
+        var quarterlySharpe = SharpeCalculator.CalculateDailySharpe(index.Select(i => i.Close), 62);
+
+        var yearlySharpeWithDate = index.Skip(250).Zip(yearlySharpe, (i, y) => new PricesWithReturns(i.Date, i.Close, y, 0, 0)).ToArray();
+        var quarterlySharpeWithDate = index.Skip(62).Zip(quarterlySharpe, (i, y) => new PricesWithReturns(i.Date, i.Close, y, 0, 0)).ToArray();
+
+        var savingTasks = new[]
+        {
+            Plot($"{fileName} yearly Sharpe ratio.png", yearlySharpeWithDate, i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.DailyGross),
+            Plot($"{fileName} quarterly Sharpe ratio.png", yearlySharpeWithDate, i => i.Date.ToDateTime(TimeOnly.MinValue), i => i.DailyGross),
+        };
+        await Task.WhenAll(savingTasks);
+    }
+
+    private static async Task Plot<T>(string name,
+        T[] data,
+        Func<T, DateTime> projectionX,
+        Func<T, decimal> projectionY,
+        Func<T, bool>? filter = null)
     {
         await Task.Yield();
         const int width = 3200, height = 1600;
 
-        var filteredData = filter is not null ? data.Where(i => filter(i.Date)).ToArray() : data;
+        var filteredData = filter is not null ? data.Where(filter).ToArray() : data;
 
         var plot = new ScottPlot.Plot();
         plot.Add.Scatter
         (
-            filteredData.Select(i => i.Date.ToDateTime(TimeOnly.MinValue)).ToArray(),
-            filteredData.Select(projection).ToArray()
+            filteredData.Select(projectionX).ToArray(),
+            filteredData.Select(projectionY).ToArray()
         );
         plot.Axes.DateTimeTicksBottom();
         plot.SavePng(name, width, height);
