2af1a90ecb43e2106b7aa97b7b4a327454b68231
Illia Levandovskyi <lis2889@gmail.com>
2026-03-04 00:08:30 +0100

    elevator problem - states calculator

---
+++ src/Udemy.Fin.Stat/ElevatorProblemStateCounter.cs
@@ -0,0 +1,16 @@
+﻿namespace Udemy.Fin.Stat;
+
+public static class ElevatorProblemStateCounter
+{
+    public static long GetStates(int floors, int people)
+    {
+        if (people == 0) return 0;
+        if (floors == 1) return 1;
+        if (people == 1) return floors;
+
+        return floors * Enumerable.Range(1, people)
+            .Select(leaving => GetStates(floors - 1, people - leaving))
+            .Sum();
+    }
+}
+
---
+++ test/Udemy.Fin.Stat.Tests/ElevatorProblemStateCounterTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+
+namespace Udemy.Fin.Stat.Tests;
+
+[Trait("Category", "Unit")]
+public class ElevatorProblemStateCounterTests
+{
+    [Fact]
+    public void GetStates_9n9_Observe()
+    {
+        var states = ElevatorProblemStateCounter.GetStates(9, 9);
+        states.Should().Be(9L * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1);
+        //found 9_426_384
+    }
+}
