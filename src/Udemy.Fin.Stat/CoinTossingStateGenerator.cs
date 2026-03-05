369176be696acd13b4e0477d951fb55a8fd21293
Illia Levandovskyi <lis2889@gmail.com>
2026-03-05 22:29:16 +0100

    generating fair and biased coin tossing states

---
+++ src/Udemy.Fin.Stat/CoinTossingStateGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Udemy.Fin.Stat;
+
+public static partial class CoinTossingStateGenerator
+{
+    public const char Heads = 'H';
+    public const char Tails = 'T';
+
+    [GeneratedRegex("H{3}", RegexOptions.Compiled, 10)]
+    private static partial Regex Get3HeadsInARow();
+
+    public static bool Has3HeadsInARow(this ReadOnlySpan<char> tossingState)
+        => Get3HeadsInARow().IsMatch(tossingState);
+
+    public static IEnumerable<string> GenerateFairCoinTosses(int counter = 1)
+    {
+        if (counter <= 1) return [$"{Heads}", $"{Tails}"];
+
+        var previous = GenerateFairCoinTosses(counter - 1).ToArray();
+        return
+        [
+            ..previous.Select(state => $"{state}{Heads}"),
+            ..previous.Select(state => $"{state}{Tails}")
+        ];
+    }
+
+    public static IEnumerable<string> GenerateBiasedCoinTosses(int counter = 1, double pHead = 0.5)
+    {
+        if (counter <= 1) return [$"{GetToss(pHead)}", $"{GetToss(1 - pHead)}"];
+
+        var previous = GenerateBiasedCoinTosses(counter - 1, pHead).ToArray();
+        return
+        [
+            ..previous.Select(state => $"{state}{GetToss(pHead)}"),
+            ..previous.Select(state => $"{state}{GetToss(1 - pHead)}")
+        ];
+    }
+
+    private static char GetToss(double pHead) => Random.Shared.NextDouble() <= pHead ? Heads : Tails;
+}
---
+++ test/Udemy.Fin.Stat.Tests/CoinTossingStateGeneratorTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using static Udemy.Fin.Stat.CoinTossingStateGenerator;
+
+namespace Udemy.Fin.Stat.Tests;
+
+[Trait("Category", "Unit")]
+public class CoinTossingStateGeneratorTests(ITestOutputHelper output)
+{
+    [Fact]
+    public void GenerateFairCoinTosses_3_MatchExpectations()
+    {
+        GenerateFairCoinTosses(3)
+            .Should()
+            .BeEquivalentTo(["HHH", "HHT", "HTH", "THH", "HTT", "THT", "TTH", "TTT"]);
+    }
+
+    [Fact]
+    public void GenerateFairCoinTosses_5_Observe()
+    {
+        var tosses = GenerateFairCoinTosses(5).ToArray();
+        tosses.Should().HaveCount(1 << 5, "growth exponentially N = 2^tosses");
+
+        foreach (var toss in tosses)
+            output.WriteLine(toss);
+
+        var probabilityFirstTails = tosses.Count(t => t[0] == Tails) * 1.0 / tosses.Length;
+        var probabilityFirstHeads = tosses.Count(t => t[0] == Heads) * 1.0 / tosses.Length;
+        var probability2n4Heads = tosses
+            .Count(t => t[1] == Heads && t[3] == Heads)
+            * 1.0 / tosses.Length;
+        var probability1n3n5Tails = tosses
+            .Count(t => t[0] == Tails && t[2] == Tails && t[4] == Tails) 
+            * 1.0 / tosses.Length;
+        var probabilityTreeHeadsInARow = tosses.Count(t => t.Has3HeadsInARow()) * 1.0 / tosses.Length;
+
+        output.WriteLine(
+            $"p(first tails) = {probabilityFirstTails};" +
+            $" p(first heads) = {probabilityFirstHeads};" +
+            $" p(3 heads in a row) = {probabilityTreeHeadsInARow};" +
+            $" p(2 & 4 heads) = {probability2n4Heads};" +
+            $" p(1&3&5 tails) = {probability1n3n5Tails}"
+        );
+    }
+
+    [Fact]
+    public void GenerateBiasedCoinTosses_4Times_Head85_Observe()
+    {
+        var tosses = GenerateBiasedCoinTosses(4, 0.85).ToArray();
+        tosses.Should().HaveCount(1 << 4, "growth exponentially N = 2^tosses");
+
+        foreach (var toss in tosses)
+            output.WriteLine(toss);
+
+        var probabilityFirstTails = tosses.Count(t => t[0] == Tails) * 1.0 / tosses.Length;
+        var probabilityFirstHeads = tosses.Count(t => t[0] == Heads) * 1.0 / tosses.Length;
+        var probability2n4Heads = tosses
+            .Count(t => t[1] == Heads && t[3] == Heads)
+            * 1.0 / tosses.Length;
+        var probabilityTreeHeadsInARow = tosses.Count(t => t.Has3HeadsInARow()) * 1.0 / tosses.Length;
+
+        output.WriteLine(
+            $"p(first tails) = {probabilityFirstTails};" +
+            $" p(first heads) = {probabilityFirstHeads};" +
+            $" p(3 heads in a row) = {probabilityTreeHeadsInARow};" +
+            $" p(2 & 4 heads) = {probability2n4Heads};"
+        );
+    }
+}
