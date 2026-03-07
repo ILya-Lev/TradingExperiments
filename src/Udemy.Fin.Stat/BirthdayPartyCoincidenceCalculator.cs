36920ce7dab1f1bddb7896f58eac1823ccf9afca
Illia Levandovskyi <lis2889@gmail.com>
2026-03-07 10:10:10 +0100

    add same bday probab calculation

---
+++ src/Udemy.Fin.Stat/BirthdayPartyCoincidenceCalculator.cs
@@ -0,0 +1,29 @@
+﻿namespace Udemy.Fin.Stat;
+
+public static class BirthdayPartyCoincidenceCalculator
+{
+    //O(n^2) - nested loop to get the product
+    public static IEnumerable<double> Generate2OrMoreSameDaySeries(int n)
+        => Enumerable.Range(1, n).Select(Calculate2OrMoreSameDay);
+
+    public static double Calculate2OrMoreSameDay(int n)
+        => 1.0 - CalculateDifferentBirthdaysProbability(n);
+
+    public static double CalculateDifferentBirthdaysProbability(int n)
+        => Enumerable.Range(0, n)
+            .Select(i => (365 - i) / 365.0)
+            .Aggregate(1.0, (acc, c) => acc * c);
+
+    //O(n) - no nested loops
+    public static IEnumerable<double> Generate2OrMoreSameDaySeriesFast(int n)
+    {
+        var occupations = Enumerable.Range(0, n).Select(i => (365 - i) / 365.0).ToArray();
+        var pAllDifferent = 1.0;
+        foreach (var pOneDifferent in occupations)
+        {
+            pAllDifferent *= pOneDifferent;
+            yield return 1 - pAllDifferent;
+        }
+    }
+}
+
---
+++ test/Udemy.Fin.Stat.Tests/BirthdayPartyCoincidenceCalculatorTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+
+namespace Udemy.Fin.Stat.Tests;
+
+[Trait("Category", "Unit")]
+public class BirthdayPartyCoincidenceCalculatorTests(ITestOutputHelper output)
+{
+    [Fact]
+    public void Generate2OrMoreSameDay_65_Observe()
+    {
+        BirthdayPartyCoincidenceCalculator.Calculate2OrMoreSameDay(65)
+            .Should().BeApproximately(0.99768, 1e-5);
+    }
+
+    [Fact]
+    public void Generate2OrMoreSameDay_50_Observe()
+    {
+        BirthdayPartyCoincidenceCalculator.Calculate2OrMoreSameDay(50)
+            .Should().BeApproximately(0.97, 1e-3);
+    }
+
+    [Fact]
+    public void Generate2OrMoreSameDay_30_Observe()
+    {
+        BirthdayPartyCoincidenceCalculator.Calculate2OrMoreSameDay(30)
+            .Should().BeApproximately(0.706, 1e-3);
+    }
+
+    [Fact]
+    public async Task Generate2OrMoreSameDaySeries_50_Observe()
+    {
+        var series = BirthdayPartyCoincidenceCalculator
+            .Generate2OrMoreSameDaySeries(350)
+            .ToArray();
+        
+        series.Should().NotBeEmpty();
+        await Plot("at least 2 bdays at the same day.png", series);
+    }
+
+    [Fact]
+    public async Task Generate2OrMoreSameDaySeriesFast_65_Observe()
+    {
+        var series = BirthdayPartyCoincidenceCalculator
+            .Generate2OrMoreSameDaySeriesFast(65)
+            .ToArray();
+        
+        series.Should().NotBeEmpty();
+        await Plot("at least 2 bdays at the same day fast.png", series);
+    }
+
+    private static async Task Plot<T>(string name,
+        T[] data,
+        Func<T, bool>? filter = null)
+    {
+        await Task.Yield();
+        const int width = 3200, height = 1600;
+
+        var filteredData = filter is not null ? data.Where(filter).ToArray() : data;
+
+        var plot = new ScottPlot.Plot();
+        plot.Add.Scatter
+        (
+            filteredData.Select((_, i) => i).ToArray(),
+            filteredData
+        );
+        plot.SavePng(name, width, height);
+    }
+}
