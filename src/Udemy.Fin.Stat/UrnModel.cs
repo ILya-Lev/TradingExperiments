0b7968a6e4fa92b263a51291e39d114ca290d243
Illia Levandovskyi <lis2889@gmail.com>
2026-03-08 18:57:11 +0100

    added parallelization to urn model progressing

--- src/Udemy.Fin.Stat/UrnModel.cs
+++ src/Udemy.Fin.Stat/UrnModel.cs
@@ -1,39 +1,47 @@
-﻿namespace Udemy.Fin.Stat;
+﻿using System.Collections.Concurrent;
+
+namespace Udemy.Fin.Stat;
 
 public class UrnModel(int[] State, int[][] Deltas)
 {
-    public double GetProbabilityOfPickingColorAt(int colorIndex, int attempt)
+    private static readonly ParallelOptions _parallelismOptions = new()
+    {
+        MaxDegreeOfParallelism = Environment.ProcessorCount - 1
+    };
+
+    public IReadOnlyList<double> GetProbabilityOfPickingColorAt(int attempt)
     {
         var states = PickOne()
             .Select((el, idx) => (el, idx))
             .ToDictionary
             (
                 item => item.idx, //color
-                item => new[] { item.el }.ToList()
+                item => new ConcurrentBag<(double transferProbability, UrnModel state)>() { item.el }
             );
         
         for (int generation = 1; generation < attempt; generation++)
         {
-            var nextStates = states.ToDictionary(p => p.Key, p => new List<(double, UrnModel)>());
+            var nextStates = states.ToDictionary(p => p.Key, p => new ConcurrentBag<(double, UrnModel)>());
 
             foreach (var (_, stateChain) in states)
             {
-                foreach (var (tp, state) in stateChain)
+                Parallel.ForEach(stateChain, _parallelismOptions, (pair, loopState, index) =>
+                    //foreach (var (tp, state) in pair.Value)
                 {
-                    var transfers = state.PickOne().ToArray();
+                    var transfers = pair.state.PickOne().ToArray();
 
                     for (int color = 0; color < transfers.Length; color++)
                     {
-                        var nextTransferProbability = tp * transfers[color].transferProbability;
-                        nextStates[color].Add((nextTransferProbability, transfers[color].nextState));
+                        var np = pair.transferProbability * transfers[color].transferProbability;
+                        nextStates[color].Add((np, transfers[color].nextState));
                     }
-                }
+                });
             }
 
             states = nextStates;
         }
 
-        return states[colorIndex].Sum(transfer => transfer.transferProbability);
+        return states.Select(p => p.Value.Sum(transfer => transfer.transferProbability)).ToArray();
     }
 
     public IEnumerable<(double transferProbability, UrnModel nextState)> PickOne()
--- test/Udemy.Fin.Stat.Tests/UrnModelTests.cs
+++ test/Udemy.Fin.Stat.Tests/UrnModelTests.cs
@@ -11,64 +11,64 @@ public class UrnModelTests(ITestOutputHelper output)
     {
         var urnModel = new UrnModel([20, 30], [[2, 1], [0, 2]]);
 
-        var redAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 1);
-        var blueAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 1);
-    
-        var redAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 2);
-        var blueAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 2);
-
-        using var _ = new AssertionScope();
-        redAt1AttemptProbability.Should().Be(0.4);
-        blueAt1AttemptProbability.Should().Be(0.6);
+        var firstAttempt = urnModel.GetProbabilityOfPickingColorAt(1);
+        var secondAttempt = urnModel.GetProbabilityOfPickingColorAt(2);
         
-        redAt2AttemptProbability.Should().BeApproximately(0.3968, 1e-4);
-        blueAt2AttemptProbability.Should().BeApproximately(0.6032, 1e-4);
+        using var _ = new AssertionScope();
+        firstAttempt.Should().HaveElementAt(0, 0.4);
+        firstAttempt.Should().HaveElementAt(1, 0.6);
+
+        secondAttempt[0].Should().BeApproximately(0.3968, 1e-4);
+        secondAttempt[1].Should().BeApproximately(0.6032, 1e-4);
     }
 
     [Fact]
     public void GetProbabilityOfPickingColorAt_Any10All_2ndPick()
     {
-        const int d = 10;
-        var urnModel = new UrnModel([10, 15, 25], [[d, d, d], [d, d, d], [d, d, d]]);
+        const int d = 1123;//could be any - in fact is a scale for the identity matrix
+        var urnModel = new UrnModel([10, 15, 25], [[d, 0, 0], [0, d, 0], [0, 0, d]]);
 
-        var blueAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 1);
-        var redAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 1);
-        var greenAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(2, 1);
-     
-        var blueAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 2);
-        var redAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 2);
-        var greenAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(2, 2);
-
-        using var _ = new AssertionScope();
-        blueAt1AttemptProbability.Should().Be(0.2);
-        redAt1AttemptProbability.Should().Be(0.3);
-        greenAt1AttemptProbability.Should().Be(0.5);
+        var firstAttempt = urnModel.GetProbabilityOfPickingColorAt(1);
+        var secondAttempt = urnModel.GetProbabilityOfPickingColorAt(2);
         
-        blueAt2AttemptProbability.Should().Be(0.2);
-        redAt2AttemptProbability.Should().Be(0.3);
-        greenAt2AttemptProbability.Should().Be(0.5);
+        using var _ = new AssertionScope();
+        firstAttempt.Should().HaveElementAt(0, 0.2);
+        firstAttempt.Should().HaveElementAt(1, 0.3);
+        firstAttempt.Should().HaveElementAt(2, 0.5);
+
+        secondAttempt.Should().HaveElementAt(0, 0.2);
+        secondAttempt.Should().HaveElementAt(1, 0.3);
+        secondAttempt.Should().HaveElementAt(2, 0.5);
     }
 
     [Fact]
-    public void GetProbabilityOfPickingColorAt_R10r_B5b5r_G10b5g_2ndPick()
+    public void GetProbabilityOfPickingColorAt_R10r_B5b5r_G5b5g_2ndPick()
     {
-        var urnModel = new UrnModel([10, 15, 25], [[5,5,0], [0, 10, 0], [5, 0, 5]]);
+        var urnModel = new UrnModel([10, 15, 25], [[5, 5, 0], [0, 10, 0], [5, 0, 5]]);
 
-        var blueAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 1);
-        var redAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 1);
-        var greenAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(2, 1);
-     
-        var blueAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 2);
-        var redAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 2);
-        var greenAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(2, 2);
+        var firstAttempt = urnModel.GetProbabilityOfPickingColorAt( 1);
+        var secondAttempt = urnModel.GetProbabilityOfPickingColorAt( 2);
 
         using var _ = new AssertionScope();
-        blueAt1AttemptProbability.Should().Be(0.2);
-        redAt1AttemptProbability.Should().Be(0.3);
-        greenAt1AttemptProbability.Should().Be(0.5);
-        
-        blueAt2AttemptProbability.Should().BeApproximately(0.225, 1e-3);
-        redAt2AttemptProbability.Should().BeApproximately(0.3167, 1e-4);
-        greenAt2AttemptProbability.Should().BeApproximately(0.4583, 1e-4);
+        firstAttempt.Should().HaveElementAt(0, 0.2);
+        firstAttempt.Should().HaveElementAt(1, 0.3);
+        firstAttempt.Should().HaveElementAt(2, 0.5);
+
+        secondAttempt[0].Should().BeApproximately(0.225, 1e-3);
+        secondAttempt[1].Should().BeApproximately(0.3167, 1e-4);
+        secondAttempt[2].Should().BeApproximately(0.4583, 1e-4);
+    }
+
+    [Fact]//growth exponentially with base 3 and power of iterations: 3^15 ~ 14M - should be affordable
+    public void GetProbabilityOfPickingColorAt_R10r_B5b5r_G5b5g_50thPick()
+    {
+        var urnModel = new UrnModel([10, 15, 25], [[5, 5, 0], [0, 10, 0], [5, 0, 5]]);
+
+        var theAttempt = urnModel.GetProbabilityOfPickingColorAt(15);
+
+        using var _ = new AssertionScope();
+        theAttempt[0].Should().BeApproximately(0.2787, 1e-4);
+        theAttempt[1].Should().BeApproximately(0.4601, 1e-4);
+        theAttempt[2].Should().BeApproximately(0.2613, 1e-4);
     }
 }
