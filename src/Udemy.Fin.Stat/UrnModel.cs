698a2f4504313dc803dbfbf49f412d008a4fb655
Illia Levandovskyi <lis2889@gmail.com>
2026-03-08 13:03:23 +0100

    urn model added - wip

---
+++ src/Udemy.Fin.Stat/UrnModel.cs
@@ -0,0 +1,49 @@
+﻿namespace Udemy.Fin.Stat;
+
+public class UrnModel(int[] State, int[][] Deltas)
+{
+    public double GetProbabilityOfPickingColorAt(int colorIndex, int attempt)
+    {
+        var states = PickOne()
+            .Select((el, idx) => (el, idx))
+            .ToDictionary
+            (
+                item => item.idx, //color
+                item => new[] { item.el }.ToList()
+            );
+        
+        for (int generation = 1; generation < attempt; generation++)
+        {
+            var nextStates = states.ToDictionary(p => p.Key, p => new List<(double, UrnModel)>());
+
+            foreach (var (_, stateChain) in states)
+            {
+                foreach (var (tp, state) in stateChain)
+                {
+                    var transfers = state.PickOne().ToArray();
+
+                    for (int color = 0; color < transfers.Length; color++)
+                    {
+                        var nextTransferProbability = tp * transfers[color].transferProbability;
+                        nextStates[color].Add((nextTransferProbability, transfers[color].nextState));
+                    }
+                }
+            }
+
+            states = nextStates;
+        }
+
+        return states[colorIndex].Sum(transfer => transfer.transferProbability);
+    }
+
+    public IEnumerable<(double transferProbability, UrnModel nextState)> PickOne()
+    {
+        var total = State.Sum() * 1.0;
+        for (int i = 0; i < State.Length; i++)
+        {
+            var transferProbability = State[i] / total;
+            var nextState = State.Zip(Deltas[i], (s, d) => s + d).ToArray();
+            yield return (transferProbability, new UrnModel(nextState, Deltas));
+        }
+    }
+}
---
+++ test/Udemy.Fin.Stat.Tests/UrnModelTests.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace Udemy.Fin.Stat.Tests;
+
+[Trait("Category", "Unit")]
+public class UrnModelTests(ITestOutputHelper output)
+{
+    [Fact]
+    public void GetProbabilityOfPickingColorAt_Red2And1_Blue0And2_2ndPick()
+    {
+        var urnModel = new UrnModel([20, 30], [[2, 1], [0, 2]]);
+
+        var redAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 1);
+        var blueAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 1);
+    
+        var redAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 2);
+        var blueAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 2);
+
+        using var _ = new AssertionScope();
+        redAt1AttemptProbability.Should().Be(0.4);
+        blueAt1AttemptProbability.Should().Be(0.6);
+        
+        redAt2AttemptProbability.Should().BeApproximately(0.3968, 1e-4);
+        blueAt2AttemptProbability.Should().BeApproximately(0.6032, 1e-4);
+    }
+
+    [Fact]
+    public void GetProbabilityOfPickingColorAt_Any10All_2ndPick()
+    {
+        const int d = 10;
+        var urnModel = new UrnModel([10, 15, 25], [[d, d, d], [d, d, d], [d, d, d]]);
+
+        var blueAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 1);
+        var redAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 1);
+        var greenAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(2, 1);
+     
+        var blueAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 2);
+        var redAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 2);
+        var greenAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(2, 2);
+
+        using var _ = new AssertionScope();
+        blueAt1AttemptProbability.Should().Be(0.2);
+        redAt1AttemptProbability.Should().Be(0.3);
+        greenAt1AttemptProbability.Should().Be(0.5);
+        
+        blueAt2AttemptProbability.Should().Be(0.2);
+        redAt2AttemptProbability.Should().Be(0.3);
+        greenAt2AttemptProbability.Should().Be(0.5);
+    }
+
+    [Fact]
+    public void GetProbabilityOfPickingColorAt_R10r_B5b5r_G10b5g_2ndPick()
+    {
+        var urnModel = new UrnModel([10, 15, 25], [[5,5,0], [0, 10, 0], [5, 0, 5]]);
+
+        var blueAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 1);
+        var redAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 1);
+        var greenAt1AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(2, 1);
+     
+        var blueAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(0, 2);
+        var redAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(1, 2);
+        var greenAt2AttemptProbability = urnModel.GetProbabilityOfPickingColorAt(2, 2);
+
+        using var _ = new AssertionScope();
+        blueAt1AttemptProbability.Should().Be(0.2);
+        redAt1AttemptProbability.Should().Be(0.3);
+        greenAt1AttemptProbability.Should().Be(0.5);
+        
+        blueAt2AttemptProbability.Should().BeApproximately(0.225, 1e-3);
+        redAt2AttemptProbability.Should().BeApproximately(0.3167, 1e-4);
+        greenAt2AttemptProbability.Should().BeApproximately(0.4583, 1e-4);
+    }
+}
