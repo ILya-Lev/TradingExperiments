1d0d54e1c4af3c4b1671cf7bdbcad69b3664f87f
Illia Levandovskyi <lis2889@gmail.com>
2026-03-09 22:10:05 +0100

    added overall correctness check for urn model tests

--- test/Udemy.Fin.Stat.Tests/UrnModelTests.cs
+++ test/Udemy.Fin.Stat.Tests/UrnModelTests.cs
@@ -17,9 +17,11 @@ public class UrnModelTests(ITestOutputHelper output)
         using var _ = new AssertionScope();
         firstAttempt.Should().HaveElementAt(0, 0.4);
         firstAttempt.Should().HaveElementAt(1, 0.6);
+        firstAttempt.Sum().Should().BeApproximately(1, 1e-4);
 
         secondAttempt[0].Should().BeApproximately(0.3968, 1e-4);
         secondAttempt[1].Should().BeApproximately(0.6032, 1e-4);
+        secondAttempt.Sum().Should().BeApproximately(1, 1e-4);
     }
 
     [Fact]
@@ -35,10 +37,12 @@ public class UrnModelTests(ITestOutputHelper output)
         firstAttempt.Should().HaveElementAt(0, 0.2);
         firstAttempt.Should().HaveElementAt(1, 0.3);
         firstAttempt.Should().HaveElementAt(2, 0.5);
+        firstAttempt.Sum().Should().BeApproximately(1, 1e-4);
 
         secondAttempt.Should().HaveElementAt(0, 0.2);
         secondAttempt.Should().HaveElementAt(1, 0.3);
         secondAttempt.Should().HaveElementAt(2, 0.5);
+        secondAttempt.Sum().Should().BeApproximately(1, 1e-4);
     }
 
     [Fact]
@@ -53,10 +57,12 @@ public class UrnModelTests(ITestOutputHelper output)
         firstAttempt.Should().HaveElementAt(0, 0.2);
         firstAttempt.Should().HaveElementAt(1, 0.3);
         firstAttempt.Should().HaveElementAt(2, 0.5);
+        firstAttempt.Sum().Should().BeApproximately(1, 1e-4);
 
         secondAttempt[0].Should().BeApproximately(0.225, 1e-3);
         secondAttempt[1].Should().BeApproximately(0.3167, 1e-4);
         secondAttempt[2].Should().BeApproximately(0.4583, 1e-4);
+        secondAttempt.Sum().Should().BeApproximately(1, 1e-4);
     }
 
     [Fact]//growth exponentially with base 3 and power of iterations: 3^15 ~ 14M - should be affordable
@@ -70,5 +76,6 @@ public class UrnModelTests(ITestOutputHelper output)
         theAttempt[0].Should().BeApproximately(0.2787, 1e-4);
         theAttempt[1].Should().BeApproximately(0.4601, 1e-4);
         theAttempt[2].Should().BeApproximately(0.2613, 1e-4);
+        theAttempt.Sum().Should().BeApproximately(1, 1e-4);
     }
 }
