8371d8558145a34f7d2e868b49f011d58067a9b3
Illia Levandovskyi <lis2889@gmail.com>
2026-03-10 23:00:28 +0100

    added combinations number calculation and discrete random variable calculation - tossing a coin

---
+++ src/Udemy.Fin.Stat/DiscreteFlippingProbabilityCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace Udemy.Fin.Stat;
+
+public static class DiscreteFlippingProbabilityCalculator
+{
+    public static double GetOccurrencesProbability(int flips, int occurrences, double headsProbability = 0.5) =>
+        flips.GetCombinationsNumber(occurrences)
+        * Math.Pow(headsProbability, occurrences)
+        * Math.Pow(1 - headsProbability, flips - occurrences);
+
+    public static double GetCombinationsNumber(this int n, int k)
+    {
+        var result = 1.0;
+        var min = k < (n - k) ? k : (n - k);
+        for (int i = 0; i < min; i++)
+        {
+            result *= 1.0 * (n - i) / (min - i);
+        }
+        return result;
+    }
+}
---
+++ test/Udemy.Fin.Stat.Tests/DiscreteFlippingProbabilityCalculatorTests.cs
@@ -0,0 +1,83 @@
+﻿namespace Udemy.Fin.Stat.Tests;
+
+[Trait("Category", "Unit")]
+public class DiscreteFlippingProbabilityCalculatorTests(ITestOutputHelper output)
+{
+    [Fact]
+    public async Task GetOccurrencesProbability_FairFourTosses_PlotDistribution()
+    {
+        const int flips = 4;
+
+        var distribution =
+            Enumerable.Range(0, flips + 1)
+            .ToDictionary
+            (
+                c => c,
+                c => DiscreteFlippingProbabilityCalculator.GetOccurrencesProbability(flips, c)
+            );
+
+        foreach (var (times, p) in distribution)
+        {
+            output.WriteLine($"tossing 4 times can result {times} times head in fair coin w probability {p:P2}");
+        }
+
+        await Plot("fair coin 4 times.png", distribution.Values.ToArray());
+    }
+
+    [Fact]
+    public async Task GetOccurrencesProbability_BiasedFourTosses_PlotDistribution()
+    {
+        const int flips = 4;
+
+        var distribution =
+            Enumerable.Range(0, flips + 1)
+            .ToDictionary
+            (
+                c => c,
+                c => DiscreteFlippingProbabilityCalculator.GetOccurrencesProbability(flips, c, 0.65)
+            );
+
+        foreach (var (times, p) in distribution)
+        {
+            output.WriteLine($"tossing 4 times can result {times} times head in fair coin w probability {p:P2}");
+        }
+
+        await Plot("biased 65p heads coin 4 times.png", distribution.Values.ToArray());
+    }
+
+    [Fact]
+    public async Task GetOccurrencesProbability_BiasedHundredTosses_PlotDistribution()
+    {
+        const int flips = 100;
+
+        var distribution =
+            Enumerable.Range(0, flips + 1)
+            .ToDictionary
+            (
+                c => c,
+                c => DiscreteFlippingProbabilityCalculator.GetOccurrencesProbability(flips, c, 0.65)
+            );
+
+        foreach (var (times, p) in distribution)
+        {
+            output.WriteLine($"tossing 4 times can result {times} times head in fair coin w probability {p:P2}");
+        }
+
+        await Plot("biased 65p heads coin 100 times.png", distribution.Values.ToArray());
+    }
+
+    private static async Task Plot<T>(string name, T[] data)
+    {
+        await Task.Yield();
+        const int width = 3200, height = 1600;
+
+
+        var plot = new ScottPlot.Plot();
+        plot.Add.Scatter
+        (
+            data.Select((_, i) => i).ToArray(),
+            data
+        );
+        plot.SavePng(name, width, height);
+    }
+}
