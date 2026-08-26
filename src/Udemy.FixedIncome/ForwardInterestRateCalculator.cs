8774d4ec48a79cbdcc11de047650df7707a3b69d
Illia Levandovskyi <lis2889@gmail.com>
2026-08-27 01:01:48 +0200

    semiannually compounded implied spot curve

--- src/Udemy.FixedIncome/ForwardInterestRateCalculator.cs
+++ src/Udemy.FixedIncome/ForwardInterestRateCalculator.cs
@@ -2,40 +2,48 @@
 
 public static class ForwardInterestRateCalculator
 {
-    public static double GetForwardRate(double r1, double t1, double r2, double t2) => 
-        Math.Pow((Math.Pow(1+r2, t2))/(Math.Pow(1+r1, t1)), 1 / (t2 - t1)) - 1;
+    public static double GetForwardRate(double r1, double t1, double r2, double t2, int compoundingFrequency = 1) =>
+        compoundingFrequency * (
+            Math.Pow((Math.Pow(1 + r2 / compoundingFrequency, t2))
+                     / (Math.Pow(1 + r1 / compoundingFrequency, t1)),
+                1.0 / (t2 - t1))
+            - 1);
 
-    public static Dictionary<double, double> GetImpliedSpotCurve(Dictionary<double, double> interestRatesPerYear, double t1)
+    public static Dictionary<double, double> GetImpliedSpotCurve(
+        Dictionary<double, double> interestRatesPerYear
+        , double t1
+        , int compoundingFrequency = 1)
     {
-        using var iterator = interestRatesPerYear.GetEnumerator();
+        if(!interestRatesPerYear.TryGetValue(t1, out var r1))
+            return [];
 
-        var r1 = 0.0;
-        while (iterator.MoveNext())
-        {
-            if (Math.Abs(iterator.Current.Key - t1) < 1e-6)
-            {
-                r1 = iterator.Current.Value;
-                break;
-            }
-        }
+        var step = 1.0 / compoundingFrequency;
+        var t2 = t1 + step;
 
-        if (r1 == 0) return [];
-        
         var impliedSpotCurve = new Dictionary<double, double>();
-        while (iterator.MoveNext())
+
+        while (interestRatesPerYear.TryGetValue(t2, out var r2))
         {
-            var r2 = iterator.Current.Value;
-            var t2 = iterator.Current.Key;
-
-            var f = GetForwardRate(r1, t1, r2, t2);
-            impliedSpotCurve.Add(t2,f);
+            var f = GetForwardRate(r1, t1, r2, t2, compoundingFrequency);
+            impliedSpotCurve.Add(t2, f);
+            t2 += step;
         }
-
+        
         return impliedSpotCurve;
     }
 
-    public static IEnumerable<(double t1, double t2, double f)> GetOneYearForwardRates(Dictionary<double, double> interestRatesPerYear)
-        => interestRatesPerYear.SkipLast(1)
-            .Zip(interestRatesPerYear.Skip(1),
-                (lhs, rhs) => (lhs.Key, rhs.Key, GetForwardRate(lhs.Value, lhs.Key, rhs.Value, rhs.Key)));
+    public static IEnumerable<(double t1, double t2, double f)> GetForwardRatesShifted(
+        Dictionary<double, double> interestRatesPerYear
+        , double shift = 1.0
+        , int compoundingFrequency = 1)
+        => interestRatesPerYear
+            .Select(p =>
+            {
+                var t2 = p.Key + shift;
+                if (!interestRatesPerYear.TryGetValue(t2, out var r2))
+                    return default((double t1, double t2, double f)?);
+                return (p.Key, t2, GetForwardRate(p.Value, p.Key, r2, t2, compoundingFrequency));
+            })
+            .Where(item => item is not null)
+            .Select(item => item!.Value);
 }
--- test/Udemy.FixedIncome/ForwardInterestRateCalculatorTests.cs
+++ test/Udemy.FixedIncome/ForwardInterestRateCalculatorTests.cs
@@ -36,11 +36,94 @@ public class ForwardInterestRateCalculatorTests(ITestOutputHelper output)
             [5] = 0.025,
         };
 
-        var curve = ForwardInterestRateCalculator.GetOneYearForwardRates(interestRates);
+        var curve = ForwardInterestRateCalculator.GetForwardRatesShifted(interestRates);
 
         foreach (var (t1, t2, f) in curve)
         {
             output.WriteLine($"f ({t1}, {t2}) = {f:N4}");
         }
+    }
+
+    [Fact]
+    public void GetForwardRates_Task1Lecture51_Observe()
+    {
+        var interestRates = new Dictionary<double, double>()
+        {
+            [1] = 0.012,
+            [2] = 0.018,
+            [3] = 0.023,
+            [4] = 0.025,
+            [5] = 0.026,
+        };
+
+        var curve1 = ForwardInterestRateCalculator.GetImpliedSpotCurve(interestRates, 1);
+        var curve3 = ForwardInterestRateCalculator.GetImpliedSpotCurve(interestRates, 3);
+        var curve = ForwardInterestRateCalculator.GetForwardRatesShifted(interestRates);
+
+        foreach (var (t, f) in curve1)
+        {
+            output.WriteLine($"f (1, {t}) = {f:N4}");
+        }
+
+        foreach (var (t, f) in curve3)
+        {
+            output.WriteLine($"f (3, {t}) = {f:N4}");
+        }
+
+        foreach (var (t1, t2, f) in curve)
+        {
+            output.WriteLine($"f ({t1}, {t2}) = {f:N4}");
+        }
+    }
+
+    [Fact]
+    public void GetForwardRates_Task3Lecture51_Observe()
+    {
+        var interestRates = new Dictionary<double, double>()
+        {
+            [0.5] = 0.015,
+            [1.0] = 0.024,
+            [1.5] = 0.031,
+            [2.0] = 0.037,
+            [2.5] = 0.040,
+            [3.0] = 0.042,
+        };
+
+        var curve05 = ForwardInterestRateCalculator.GetImpliedSpotCurve(interestRates, 0.5, 2);
+
+        foreach (var (t, f) in curve05)
+        {
+            output.WriteLine($"f (0.5, {t:N1}) = {f:N4}");
+        }
+    }
+
+    [Fact]
+    public void GetForwardRates_Task2Lecture52_Observe()
+    {
+        var interestRates = new Dictionary<double, double>()
+        {
+            [0.5] = 0.0028,
+            [1.0] = 0.0092,
+            [1.5] = 0.0145,
+            [2.0] = 0.0195,
+            [3.0] = 0.0250,
+            [4.0] = 0.0316,
+            [5.0] = 0.0351,
+            [6.0] = 0.0393,
+            [8.0] = 0.0415,
+            [10.0] = 0.0428,
+        };
+
+        var interpolated = interestRates.Select(p => (p.Key, p.Value)).Interpolate(0.5).ToDictionary();
+
+        var curve1 = ForwardInterestRateCalculator.GetImpliedSpotCurve(interpolated, 1);
+        var curve1Semi = ForwardInterestRateCalculator.GetImpliedSpotCurve(interpolated, 1, 2);
+        var curve5 = ForwardInterestRateCalculator.GetImpliedSpotCurve(interpolated, 5);
+        var curve5Semi = ForwardInterestRateCalculator.GetImpliedSpotCurve(interpolated, 5, 2);
+
+        foreach (var (t, f) in curve1) output.WriteLine($"f (1, {t:N1}) = {f:N4}");
+        foreach (var (t, f) in curve1Semi) output.WriteLine($"f (1, {t:N1}) = {f:N4}");
+        foreach (var (t, f) in curve5) output.WriteLine($"f (5, {t:N1}) = {f:N4}");
+        foreach (var (t, f) in curve5Semi) output.WriteLine($"f (5, {t:N1}) = {f:N4}");
     }
 }
