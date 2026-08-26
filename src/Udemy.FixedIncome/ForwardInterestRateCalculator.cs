b910d51bf13b8162f06006b269a49703680ef7d2
Illia Levandovskyi <lis2889@gmail.com>
2026-08-26 20:53:15 +0200

    added implied forward rates calculator

---
+++ src/Udemy.FixedIncome/ForwardInterestRateCalculator.cs
@@ -0,0 +1,41 @@
+﻿namespace Udemy.FixedIncome;
+
+public static class ForwardInterestRateCalculator
+{
+    public static double GetForwardRate(double r1, double t1, double r2, double t2) => 
+        Math.Pow((Math.Pow(1+r2, t2))/(Math.Pow(1+r1, t1)), 1 / (t2 - t1)) - 1;
+
+    public static Dictionary<double, double> GetImpliedSpotCurve(Dictionary<double, double> interestRatesPerYear, double t1)
+    {
+        using var iterator = interestRatesPerYear.GetEnumerator();
+
+        var r1 = 0.0;
+        while (iterator.MoveNext())
+        {
+            if (Math.Abs(iterator.Current.Key - t1) < 1e-6)
+            {
+                r1 = iterator.Current.Value;
+                break;
+            }
+        }
+
+        if (r1 == 0) return [];
+        
+        var impliedSpotCurve = new Dictionary<double, double>();
+        while (iterator.MoveNext())
+        {
+            var r2 = iterator.Current.Value;
+            var t2 = iterator.Current.Key;
+
+            var f = GetForwardRate(r1, t1, r2, t2);
+            impliedSpotCurve.Add(t2,f);
+        }
+
+        return impliedSpotCurve;
+    }
+
+    public static IEnumerable<(double t1, double t2, double f)> GetOneYearForwardRates(Dictionary<double, double> interestRatesPerYear)
+        => interestRatesPerYear.SkipLast(1)
+            .Zip(interestRatesPerYear.Skip(1),
+                (lhs, rhs) => (lhs.Key, rhs.Key, GetForwardRate(lhs.Value, lhs.Key, rhs.Value, rhs.Key)));
+}
---
+++ test/Udemy.FixedIncome/ForwardInterestRateCalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Udemy.FixedIncome.Tests;
+
+[Trait("Category", "Unit")]
+public class ForwardInterestRateCalculatorTests(ITestOutputHelper output)
+{
+
+    [Fact]
+    public void GetImpliedSpotCurve_Example2Lecture51_Observe()
+    {
+        var interestRates = new Dictionary<double, double>()
+        {
+            [1] = 0.006,
+            [2] = 0.011,
+            [3] = 0.014,
+            [4] = 0.019,
+            [5] = 0.023,
+        };
+
+        var curve = ForwardInterestRateCalculator.GetImpliedSpotCurve(interestRates, 1);
+
+        foreach (var (t,f) in curve)
+        {
+            output.WriteLine($"f (1, {t}) = {f:N4}");
+        }
+    }
+
+    [Fact]
+    public void GetOneYearForwardRates_Example3Lecture51_Observe()
+    {
+        var interestRates = new Dictionary<double, double>()
+        {
+            [1] = 0.008,
+            [2] = 0.015,
+            [3] = 0.019,
+            [4] = 0.023,
+            [5] = 0.025,
+        };
+
+        var curve = ForwardInterestRateCalculator.GetOneYearForwardRates(interestRates);
+
+        foreach (var (t1, t2, f) in curve)
+        {
+            output.WriteLine($"f ({t1}, {t2}) = {f:N4}");
+        }
+    }
+}
