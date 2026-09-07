ef1a67e74e5425e2eca68725bafb1b7524be893c
Illia Levandovskyi <lis2889@gmail.com>
2026-09-07 22:16:53 +0200

    added bond price and dollar duration calculation for flat term structure

---
+++ src/Udemy.FixedIncome/BondEstimators.cs
@@ -0,0 +1,34 @@
+﻿namespace Udemy.FixedIncome;
+
+public static class BondEstimators
+{
+    public static double GetPriceFlatTerms(
+        double faceValue, double maturityYears, double yield,
+        double coupon = 0,
+        int compoundingFrequency = 1)
+    {
+        var y = yield >= 1 ? yield / 100.0 : yield;
+
+        var totalDiscountFactor = Math.Pow(1 + y / compoundingFrequency, -compoundingFrequency * maturityYears);
+
+        return (coupon / yield * (1 - totalDiscountFactor) + totalDiscountFactor) * faceValue;
+    }
+    
+    public static double GetDollarDurationFlatTerms(
+        double faceValue, double maturityYears, double yield,
+        double coupon = 0,
+        int compoundingFrequency = 1)
+    {
+        var y = yield >= 1 ? yield / 100.0 : yield;
+
+        var totalDiscountFactor = Math.Pow(1 + y / compoundingFrequency, -compoundingFrequency * maturityYears);
+
+        return (
+                   coupon / yield / y * (1 - totalDiscountFactor)
+                   - maturityYears * coupon / yield * totalDiscountFactor / (1 + y / compoundingFrequency)
+                   + maturityYears * totalDiscountFactor
+               )
+               * faceValue;
+    }
+}
+
---
+++ test/Udemy.FixedIncome/BondEstimatorsTests.cs
@@ -0,0 +1,113 @@
+﻿namespace Udemy.FixedIncome.Tests;
+
+[Trait("Category", "Unit")]
+public class BondEstimatorsTests(ITestOutputHelper output)
+{
+    [Fact]//lecture 62, problem 1 a
+    public void InterestRateShock_DollarDurationEstimation_DifferentMaturities_CompareWithPriceRecalculation()
+    {
+        var r = 4.0;
+        var f = 500_000;
+        var c = 3;
+        var ts = new[] { 5, 10, 15, 20, 30 };
+
+        foreach (var t in ts)
+        {
+            output.WriteLine($"\t\t\tmaturity T = {t} years:");
+            var basePrice = BondEstimators.GetPriceFlatTerms(f, t, r, c);
+            var dollarDuration = BondEstimators.GetDollarDurationFlatTerms(f, t, r, c);
+
+            var rs = new[] { 1, 2, 3, 3.5, 4.5, 5, 6, 7 };
+
+            foreach (var rate in rs)
+            {
+                var estimatedPriceChange = -(rate - r) / 100.0 * dollarDuration;
+                var actualPriceChange = BondEstimators.GetPriceFlatTerms(f, t, rate, c) - basePrice;
+
+                output.WriteLine($"dr = {rate - r} %, estimated change = {estimatedPriceChange:N4}, actual change = {actualPriceChange:N4}, error = {Math.Abs(estimatedPriceChange / actualPriceChange - 1):p2}");
+            }
+        }
+    }
+
+    [Fact]//lecture 62, problem 1 b
+    public void InterestRateShock_DollarDurationEstimation_DifferentCoupons_CompareWithPriceRecalculation()
+    {
+        var r = 4.0;
+        var f = 500_000;
+        var cs = new[] { 3, 4, 5, 7, 10 };
+        var t = 5;
+
+        foreach (var c in cs)
+        {
+            output.WriteLine($"\t\t\tcoupon C = {c} %:");
+            var basePrice = BondEstimators.GetPriceFlatTerms(f, t, r, c);
+            var dollarDuration = BondEstimators.GetDollarDurationFlatTerms(f, t, r, c);
+
+            var rs = new[] { 1, 2, 3, 3.5, 4.5, 5, 6, 7 };
+
+            foreach (var rate in rs)
+            {
+                var estimatedPriceChange = -(rate - r) / 100.0 * dollarDuration;
+                var actualPriceChange = BondEstimators.GetPriceFlatTerms(f, t, rate, c) - basePrice;
+
+                output.WriteLine($"dr = {rate - r} %, estimated change = {estimatedPriceChange:N4}, actual change = {actualPriceChange:N4}, error = {Math.Abs(estimatedPriceChange / actualPriceChange - 1):p2}");
+            }
+        }
+    }
+
+    [Fact]//lecture 62, problem 1 c
+    public void InterestRateShock_DollarDurationEstimation_DifferentInterestRates_CompareWithPriceRecalculation()
+    {
+        var rs = new[] { 3, 4, 5, 7, 10 };
+        var f = 500_000;
+        var c = 3;
+        var t = 5;
+
+        foreach (var r in rs)
+        {
+            output.WriteLine($"\t\t\tinterest rate r = {r} %:");
+            var basePrice = BondEstimators.GetPriceFlatTerms(f, t, r, c);
+            var dollarDuration = BondEstimators.GetDollarDurationFlatTerms(f, t, r, c);
+
+            var drs = new[] { -3, -2, -1, -.5, .5, 1, 2, 3 };
+
+            foreach (var dr in drs)
+            {
+                var estimatedPriceChange = -dr / 100.0 * dollarDuration;
+                var actualPriceChange = BondEstimators.GetPriceFlatTerms(f, t, r+dr, c) - basePrice;
+
+                output.WriteLine($"dr = {dr} %, estimated change = {estimatedPriceChange:N4}, actual change = {actualPriceChange:N4}, error = {Math.Abs(estimatedPriceChange / actualPriceChange - 1):p2}");
+            }
+        }
+    }
+
+    [Fact]//lecture 62, problem 2
+    public void InterestRateShock_DollarDurationEstimation_CompareWithPriceRecalculation()
+    {
+        var r = 4;
+        var portfolio = new[]
+        {
+            (T:5, F:100_000, C:0),
+            (T:3, F:250_000, C:2),
+            (T:7, F:500_000, C:2/*4.5*/),
+            (T:15, F:450_000, C:5.5)
+        };
+
+        var price = portfolio.Sum(b => BondEstimators.GetPriceFlatTerms(b.F, b.T, r, b.C, 2));
+        var duration = portfolio.Sum(b => BondEstimators.GetDollarDurationFlatTerms(b.F, b.T, r, b.C, 2));
+
+        output.WriteLine($"portfolio price {price} and duration = {duration}");
+
+        var drs = new[] { -4, -1, -.5, .5, 1, 4 };
+
+        foreach (var dr in drs)
+        {
+            var estimatedPriceChange = -dr/100.0 * duration;
+            var changedPrice = portfolio.Sum(b => BondEstimators.GetPriceFlatTerms(b.F, b.T, r + dr, b.C, 2));
+            var actualPriceChange = changedPrice - price;
+
+            output.WriteLine($"dr = {dr} %; estimated {estimatedPriceChange:N4}; real {actualPriceChange:N4}; error = {Math.Abs(estimatedPriceChange/actualPriceChange - 1):N4}");
+        }
+    }
+
+}
