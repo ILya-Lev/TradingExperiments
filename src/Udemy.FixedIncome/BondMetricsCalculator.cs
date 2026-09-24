0d06f7e9b943a6274fe95f5ad01ceb7e0095f77a
Illia Levandovskyi <lis2889@gmail.com>
2026-09-24 13:45:36 +0200

    added convexity hedging coefficients

--- src/Udemy.FixedIncome/BondMetricsCalculator.cs
+++ src/Udemy.FixedIncome/BondMetricsCalculator.cs
@@ -34,4 +34,39 @@ public static class BondMetricsCalculator
 
     public static double GetBondConvexity(double r, int t, double f, double c = 0, int k = 1)
         => GetBondDollarConvexity(r, t, f, c, k) / GetBondPrice(r, t, f, c, k);
+
+    public static (double lhs, double rhs) GetDollarConvexityHedgingScales
+    (
+        double c, double d,
+        double c1, double d1,
+        double c2, double d2
+    )
+    {
+        var denominator = d1 * c2 - d2 * c1;
+        var lhs = c * d2 - d * c2;
+        var rhs = -(c * d1 - d * c1);
+
+        return (lhs / denominator, rhs / denominator);
+    }
+
+    public static (double lhs, double rhs) GetConvexityHedgingScales
+    (
+        double[] prices, double[] durations, double[] convexity
+    )
+    {
+        if (prices?.Length != 3 || durations?.Length != 3 || convexity?.Length != 3)
+        {
+            throw new ArgumentException($"each of the arrays should contain exactly 3 values in order");
+        }
+
+        var denominator = durations[1] * convexity[2] - durations[2] * convexity[1];
+        var lhs = -durations[0] * convexity[2] + durations[2] * convexity[0];
+        var rhs = durations[0] * convexity[1] - durations[1] * convexity[0];
+
+        return 
+        (
+            prices[0] * lhs / prices[1] / denominator,
+            prices[0] * rhs / prices[2] / denominator
+        );
+    }
 }
